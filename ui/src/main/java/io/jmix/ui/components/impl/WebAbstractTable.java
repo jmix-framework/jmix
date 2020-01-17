c0aa2c8e9089127464d24ae2292f23abb82b77fb
Roman Pinyazhin <pinyazhin@haulmont.com>
2020-01-17 17:14:21 +0400

    Table aggregation results are not updated after editing column cuba-platform/cubahaulmont/jmix-old#2580

--- ui/src/main/java/io/jmix/ui/components/impl/WebAbstractTable.java
+++ ui/src/main/java/io/jmix/ui/components/impl/WebAbstractTable.java
@@ -1629,7 +1629,17 @@ public abstract class WebAbstractTable<T extends com.vaadin.v7.ui.Table & CubaEn
 
     @Override
     public void tableSourcePropertyValueChanged(TableItems.ValueChangeEvent<E> event) {
+        handleAggregation();
         refreshActionsState();
+    }
+
+    protected void handleAggregation() {
+        if (isAggregatable() && aggregationCells != null) {
+            component.aggregate(new AggregationContainer.Context(getItems().getItemIds()));
+
+            // trigger aggregation repaint
+            component.markAsDirty();
+        }
     }
 
     @Override

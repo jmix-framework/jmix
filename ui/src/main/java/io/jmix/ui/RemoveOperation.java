ec4ff6d8b47100cb1ca9eeaf175ede1ac9e01fbf
Gleb Durygin <durygin@haulmont.com>
2020-02-03 16:33:38 +0400

    Improve Javadocs haulmont/jmix-old#161

--- ui/src/main/java/io/jmix/ui/RemoveOperation.java
+++ ui/src/main/java/io/jmix/ui/RemoveOperation.java
@@ -464,6 +464,9 @@ public class RemoveOperation {
         EXCLUDE
     }
 
+    /**
+     * Event sent before selected entities are removed.
+     */
     public static class BeforeActionPerformedEvent<E> extends EventObject {
         protected final List<E> items;
         protected boolean actionPrevented = false;
@@ -483,19 +486,31 @@ public class RemoveOperation {
             return (FrameOwner) super.getSource();
         }
 
+        /**
+         * @return the list of entities selected for removal
+         */
         public List<E> getItems() {
             return items;
         }
 
+        /**
+         * @return whether the removal was prevented by invoking {@link #preventAction()}
+         */
         public boolean isActionPrevented() {
             return actionPrevented;
         }
 
+        /**
+         * Prevents the removal.
+         */
         public void preventAction() {
             this.actionPrevented = true;
         }
     }
 
+    /**
+     * Event sent after selected entities are removed.
+     */
     public static class AfterActionPerformedEvent<E> extends EventObject {
         protected final List<E> items;
 
@@ -514,11 +529,17 @@ public class RemoveOperation {
             return (FrameOwner) super.getSource();
         }
 
+        /**
+         * @return the list of entities selected for removal
+         */
         public List<E> getItems() {
             return items;
         }
     }
 
+    /**
+     * Event sent when the remove operation is cancelled by user in the confirmation dialog.
+     */
     public static class ActionCancelledEvent<E> extends EventObject {
         protected final List<E> items;
 
@@ -537,6 +558,9 @@ public class RemoveOperation {
             return (FrameOwner) super.getSource();
         }
 
+        /**
+         * @return the list of entities selected for removal
+         */
         public List<E> getItems() {
             return items;
         }

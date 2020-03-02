27f63bd86739f07af1944ae8aa8e3c23708b0cab
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-03-02 09:22:15 +0400

    Move persistence-related beans to auto-configuration haulmont/jmix-old#266

--- ui/src/test/java/test_support/DataContextTestConfiguration.java
+++ ui/src/test/java/test_support/DataContextTestConfiguration.java
@@ -17,14 +17,25 @@
 package test_support;
 
 import io.jmix.core.JmixCoreConfiguration;
+import io.jmix.core.Stores;
 import io.jmix.core.annotation.JmixModule;
 import io.jmix.core.annotation.JmixProperty;
 import io.jmix.core.security.JmixCoreSecurityConfiguration;
+import io.jmix.data.impl.JmixEntityManagerFactoryBean;
+import io.jmix.data.impl.JmixTransactionManager;
+import io.jmix.data.impl.PersistenceConfigProcessor;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 @Configuration
@@ -35,11 +46,36 @@ import javax.sql.DataSource;
 public class DataContextTestConfiguration {
 
     @Bean
-    protected DataSource dataSource() {
+    DataSource dataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl("jdbc:hsqldb:mem:testdb");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
         return dataSource;
+    }
+
+    @Bean
+    @Primary
+    LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            DataSource dataSource, PersistenceConfigProcessor processor, JpaVendorAdapter jpaVendorAdapter) {
+        return new JmixEntityManagerFactoryBean(Stores.MAIN, dataSource, processor, jpaVendorAdapter);
+    }
+
+    @Bean
+    @Primary
+    PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        return new JmixTransactionManager(Stores.MAIN, entityManagerFactory);
+    }
+
+    @Bean
+    @Primary
+    JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    @Primary
+    TransactionTemplate transactionTemplate(PlatformTransactionManager transactionManager) {
+        return new TransactionTemplate(transactionManager);
     }
 }

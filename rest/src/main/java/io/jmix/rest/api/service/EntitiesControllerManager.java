827541172c209d07e04d6b5567d08a2fdafa094d
Konstantin Krivopustov <krivopustov@haulmont.com>
2020-06-22 17:43:29 +0400

    Rework handling of entities with nullable identifiers haulmont/jmix-old#512

--- rest/src/main/java/io/jmix/rest/api/service/EntitiesControllerManager.java
+++ rest/src/main/java/io/jmix/rest/api/service/EntitiesControllerManager.java
@@ -22,14 +22,11 @@ import com.google.gson.*;
 import io.jmix.core.*;
 import io.jmix.core.common.util.Preconditions;
 import io.jmix.core.entity.EntityValues;
-import io.jmix.core.entity.IdProxy;
 import io.jmix.core.impl.importexport.EntityImportException;
 import io.jmix.core.impl.importexport.EntityImportViewBuilder;
 import io.jmix.core.metamodel.model.MetaClass;
 import io.jmix.core.security.EntityOp;
 import io.jmix.core.security.Security;
-import io.jmix.data.entity.BaseIdentityIdEntity;
-import io.jmix.data.entity.BaseIntIdentityIdEntity;
 import io.jmix.rest.api.common.RestControllerUtils;
 import io.jmix.rest.api.exception.RestAPIException;
 import io.jmix.rest.api.service.filter.RestFilterParseException;
@@ -41,13 +38,13 @@ import io.jmix.rest.api.transform.JsonTransformationDirection;
 import io.jmix.rest.property.RestProperties;
 import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.annotation.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
 import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Method;
 import java.util.*;
@@ -572,46 +569,16 @@ public class EntitiesControllerManager {
 
     private Object getIdFromString(String entityId, MetaClass metaClass) {
         try {
-            if (metadataTools.hasDbGeneratedPrimaryKey(metaClass)) {
-                if (BaseIdentityIdEntity.class.isAssignableFrom(metaClass.getJavaClass())) {
-                    return IdProxy.of(Long.valueOf(entityId));
-                } else if (BaseIntIdentityIdEntity.class.isAssignableFrom(metaClass.getJavaClass())) {
-                    return IdProxy.of(Integer.valueOf(entityId));
-                } else {
-                    Class<?> clazz = metaClass.getJavaClass();
-                    while (clazz != null) {
-                        Method[] methods = clazz.getDeclaredMethods();
-                        for (Method method : methods) {
-                            if (method.getName().equals("getDbGeneratedId")) {
-                                Class<?> idClass = method.getReturnType();
-                                if (Long.class.isAssignableFrom(idClass)) {
-                                    return Long.valueOf(entityId);
-                                } else if (Integer.class.isAssignableFrom(idClass)) {
-                                    return Integer.valueOf(entityId);
-                                } else if (Short.class.isAssignableFrom(idClass)) {
-                                    return Long.valueOf(entityId);
-                                } else if (UUID.class.isAssignableFrom(idClass)) {
-                                    return UUID.fromString(entityId);
-                                }
-                            }
-                        }
-                        clazz = clazz.getSuperclass();
-                    }
-                }
-                throw new UnsupportedOperationException("Unsupported ID type in entity " + metaClass.getName());
+            Method getIdMethod = metaClass.getJavaClass().getMethod("getId");
+            Class<?> idClass = getIdMethod.getReturnType();
+            if (UUID.class.isAssignableFrom(idClass)) {
+                return UUID.fromString(entityId);
+            } else if (Integer.class.isAssignableFrom(idClass)) {
+                return Integer.valueOf(entityId);
+            } else if (Long.class.isAssignableFrom(idClass)) {
+                return Long.valueOf(entityId);
             } else {
-                //noinspection unchecked
-                Method getIdMethod = metaClass.getJavaClass().getMethod("getId");
-                Class<?> idClass = getIdMethod.getReturnType();
-                if (UUID.class.isAssignableFrom(idClass)) {
-                    return UUID.fromString(entityId);
-                } else if (Integer.class.isAssignableFrom(idClass)) {
-                    return Integer.valueOf(entityId);
-                } else if (Long.class.isAssignableFrom(idClass)) {
-                    return Long.valueOf(entityId);
-                } else {
-                    return entityId;
-                }
+                return entityId;
             }
         } catch (Exception e) {
             throw new RestAPIException("Invalid entity ID",

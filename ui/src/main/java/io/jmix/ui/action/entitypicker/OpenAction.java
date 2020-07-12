d13ada17e520dbc13d168f661204edf38031db58
Dmitry Taimanov <taimanov@haulmont.com>
2020-07-13 01:32:07 +0400

    Auditable and Soft-Delete implementation. Soft Delete Implementation haulmont/jmix-old#15
    Incomplete:
    - Legacy migration (partially)
    - Tests
    - Refactoring

--- ui/src/main/java/io/jmix/ui/action/entitypicker/OpenAction.java
+++ ui/src/main/java/io/jmix/ui/action/entitypicker/OpenAction.java
@@ -17,9 +17,9 @@
 package io.jmix.ui.action.entitypicker;
 
 import io.jmix.core.DevelopmentException;
-import io.jmix.core.Messages;
 import io.jmix.core.JmixEntity;
-import io.jmix.core.entity.SoftDelete;
+import io.jmix.core.Messages;
+import io.jmix.core.MetadataTools;
 import io.jmix.core.metamodel.model.MetaClass;
 import io.jmix.ui.Notifications;
 import io.jmix.ui.ScreenBuilders;
@@ -30,16 +30,16 @@ import io.jmix.ui.builder.EditorBuilder;
 import io.jmix.ui.component.Component;
 import io.jmix.ui.component.ComponentsHelper;
 import io.jmix.ui.component.EntityPicker;
-import io.jmix.ui.icon.JmixIcon;
 import io.jmix.ui.icon.Icons;
+import io.jmix.ui.icon.JmixIcon;
 import io.jmix.ui.meta.StudioAction;
 import io.jmix.ui.meta.StudioPropertiesItem;
 import io.jmix.ui.screen.*;
 import io.jmix.ui.sys.ActionScreenInitializer;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -67,6 +67,8 @@ public class OpenAction<E extends JmixEntity> extends BaseAction implements Enti
     protected UiProperties properties;
 
     protected ScreenBuilders screenBuilders;
+    @Autowired
+    protected MetadataTools metadataTools;
 
     protected boolean editable = true;
 
@@ -296,7 +298,7 @@ public class OpenAction<E extends JmixEntity> extends BaseAction implements Enti
 
         JmixEntity entity = entityPicker.getValue();
 
-        if (entity instanceof SoftDelete && ((SoftDelete) entity).isDeleted()) {
+        if (metadataTools.isSoftDeleted(entity)) {
             ScreenContext screenContext = ComponentsHelper.getScreenContext(entityPicker);
             Notifications notifications = screenContext.getNotifications();
 
--- ui/src/main/java/io/jmix/ui/component/impl/WebEntityLinkField.java
+++ ui/src/main/java/io/jmix/ui/component/impl/WebEntityLinkField.java
@@ -18,7 +18,6 @@ package io.jmix.ui.component.impl;
 
 import io.jmix.core.*;
 import io.jmix.core.common.event.Subscription;
-import io.jmix.core.entity.SoftDelete;
 import io.jmix.core.metamodel.datatype.Datatype;
 import io.jmix.core.metamodel.datatype.DatatypeRegistry;
 import io.jmix.core.metamodel.model.MetaClass;
@@ -316,7 +315,7 @@ public class WebEntityLinkField<V> extends WebV8AbstractField<JmixButtonField<V>
         }
 
         ScreenContext context = ComponentsHelper.getScreenContext(this);
-        if (entity instanceof SoftDelete && ((SoftDelete) entity).isDeleted()) {
+        if (metadataTools.isSoftDeleted(entity)) {
             context.getNotifications().create(Notifications.NotificationType.HUMANIZED)
                     .withCaption(messages.getMessage("OpenAction.objectIsDeleted"))
                     .show();

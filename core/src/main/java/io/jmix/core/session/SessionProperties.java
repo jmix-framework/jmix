402187169b2119373c28e301b65db8555cd3c003
zaharchenko <zaharchenko@haulmont.com>
2020-08-06 14:51:27 +0400

    Jmix sessions #haulmont/jmix-old/101

---
+++ core/src/main/java/io/jmix/core/session/SessionProperties.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2020 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.core.session;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+@ConfigurationProperties(prefix = "jmix.core.session")
+@ConstructorBinding
+public class SessionProperties {
+
+    int maximumUserSessions;
+
+    public SessionProperties(@DefaultValue("100") int maximumUserSessions) {
+        this.maximumUserSessions = maximumUserSessions;
+    }
+
+    public int getMaximumUserSessions() {
+        return maximumUserSessions;
+    }
+}

f61e074947a7eb035c15fcd64b4786fac2af5505
Dmitry Taimanov <taimanov@haulmont.com>
2020-10-21 20:01:19 +0400

    Create persistence.xml & orm.xml at build time. Persitence.xml fully reworked Haulmont/jmix-data#5

--- audit/src/test/java/test_support/AuditTestConfiguration.java
+++ audit/src/test/java/test_support/AuditTestConfiguration.java
@@ -67,8 +67,9 @@ public class AuditTestConfiguration {
     LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource,
                                                                 PersistenceConfigProcessor processor,
                                                                 JpaVendorAdapter jpaVendorAdapter,
-                                                                DbmsSpecifics dbmsSpecifics) {
-        return new JmixEntityManagerFactoryBean(Stores.MAIN, dataSource, processor, jpaVendorAdapter, dbmsSpecifics);
+                                                                DbmsSpecifics dbmsSpecifics,
+                                                                JmixModules jmixModules) {
+        return new JmixEntityManagerFactoryBean(Stores.MAIN, dataSource, processor, jpaVendorAdapter, dbmsSpecifics, jmixModules);
     }
 
     @Bean

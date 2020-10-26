0a92a61199d157b331c7c414008131f1a6dac7c5
subbotin <subbotin@haulmont.com>
2020-10-26 12:05:39 +0400

    Rename READ and UPDATE entity attribute policy actions to VIEW and MODIFY Haulmont/jmix-security#6

--- content/single-module-application/src/main/java/${project_rootPath}/security/FullAccessRole.java
+++ content/single-module-application/src/main/java/${project_rootPath}/security/FullAccessRole.java
@@ -14,8 +14,7 @@ public interface FullAccessRole {
     String ROLE_NAME = "system-full-access";
 
     @EntityPolicy(entityName = "*", actions = {EntityPolicyAction.ALL})
-    @EntityAttributePolicy(entityName = "*", attributes = "*", actions = {
-            EntityAttributePolicyAction.READ, EntityAttributePolicyAction.UPDATE})
+    @EntityAttributePolicy(entityName = "*", attributes = "*", action = MODIFY)
     @ScreenPolicy(screenIds = "*")
     @MenuPolicy(menuIds = "*")
     @SpecificPolicy(resources = "*")

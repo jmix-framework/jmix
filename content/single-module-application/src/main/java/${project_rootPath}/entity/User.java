4a5f63dd705aa1f16e59ac73fb0328621eedeaeb
Dmitry Taimanov <taimanov@haulmont.com>
2020-10-28 18:10:30 +0400

    Introduce JmixEntity annotation. fix template import Haulmont/jmix-core#41

--- content/single-module-application/src/main/java/${project_rootPath}/entity/User.java
+++ content/single-module-application/src/main/java/${project_rootPath}/entity/User.java
@@ -5,6 +5,7 @@ import io.jmix.core.entity.annotation.JmixGeneratedValue;
 import io.jmix.core.entity.annotation.SystemLevel;
 import io.jmix.core.metamodel.annotation.DependsOnProperties;
 import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
 import org.springframework.security.core.GrantedAuthority;
 
 import javax.persistence.*;

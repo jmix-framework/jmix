e2357dafb0c62bde3f481a74ff76ca87f16269de
budarov <budarov@haulmont.com>
2020-12-26 19:10:31 +0400

    add missing imports #7

--- content/single-module-application/src/main/java/${project_rootPath}/screen/main/MainScreen.java
+++ content/single-module-application/src/main/java/${project_rootPath}/screen/main/MainScreen.java
@@ -1,8 +1,12 @@
 package ${project_rootPackage}.screen.main;
 
 import io.jmix.ui.component.AppWorkArea;
+import io.jmix.ui.component.Button;
 import io.jmix.ui.component.Window;
+import io.jmix.ui.component.mainwindow.Drawer;
+import io.jmix.ui.icon.JmixIcon;
 import io.jmix.ui.screen.Screen;
+import io.jmix.ui.screen.Subscribe;
 import io.jmix.ui.screen.UiController;
 import io.jmix.ui.screen.UiDescriptor;
 import org.springframework.beans.factory.annotation.Autowired;

3467efb691cd5fa11f725518a780014bd3f98a8f
Vladislav Plakhov <v.plakhov@haulmont.com>
2021-01-15 17:30:50 +0400

    Export/Import for dynamic attributes #449
    fix compile

---
+++ core/src/main/java/io/jmix/core/MetadataExtension.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.core;
+
+import io.jmix.core.metamodel.model.MetaClass;
+import io.jmix.core.metamodel.model.MetaProperty;
+
+import java.util.Set;
+
+public interface MetadataExtension {
+
+    /**
+     * Get additional properties - interface providing access to additional properties, e.g. dynamic properties.
+     *
+     * @return set of additional properties
+     */
+    Set<MetaProperty> getAdditionalProperties(MetaClass metaClass);
+
+}

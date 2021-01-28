ce3af498b29b6588e257ea4d2795847dc150db0d
subbotin <subbotin@haulmont.com>
2021-01-28 13:34:05 +0400

    Separate Role on two different types: ResourceRole, RowLevelRole Haulmont/jmix-security#43
    Fix interface location

--- security-data/src/main/java/io/jmix/securitydata/user/AbstractDatabaseUserRepository.java
+++ security-data/src/main/java/io/jmix/securitydata/user/AbstractDatabaseUserRepository.java
@@ -20,7 +20,7 @@ import io.jmix.core.DataManager;
 import io.jmix.core.Metadata;
 import io.jmix.core.common.util.Preconditions;
 import io.jmix.core.entity.EntityValues;
-import io.jmix.core.security.GrantedAuthorityContainer;
+import io.jmix.security.authentication.GrantedAuthorityContainer;
 import io.jmix.core.security.PasswordNotMatchException;
 import io.jmix.core.security.UserManager;
 import io.jmix.core.security.UserRepository;

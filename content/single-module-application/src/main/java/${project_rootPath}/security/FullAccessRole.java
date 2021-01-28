f47c2496b77cce76758b935fbd49a1c9bbf6679d
subbotin <subbotin@haulmont.com>
2021-01-28 12:09:28 +0400

    Separate Role on two different types: ResourceRole, RowLevelRole Haulmont/jmix-security#43

--- content/single-module-application/src/main/java/${project_rootPath}/security/FullAccessRole.java
+++ content/single-module-application/src/main/java/${project_rootPath}/security/FullAccessRole.java
@@ -1,15 +1,17 @@
-package ${project_rootPackage}.security;
+package $
+
+{project_rootPackage}.security;
 
 import io.jmix.security.model.EntityAttributePolicyAction;
 import io.jmix.security.model.EntityPolicyAction;
 import io.jmix.security.role.annotation.EntityAttributePolicy;
 import io.jmix.security.role.annotation.EntityPolicy;
-import io.jmix.security.role.annotation.Role;
+import io.jmix.security.role.annotation.ResourceRole;
 import io.jmix.security.role.annotation.SpecificPolicy;
 import io.jmix.securityui.role.annotation.MenuPolicy;
 import io.jmix.securityui.role.annotation.ScreenPolicy;
 
-@Role(name = FullAccessRole.ROLE_NAME, code = FullAccessRole.ROLE_NAME)
+@ResourceRole(name = FullAccessRole.ROLE_NAME, code = FullAccessRole.ROLE_NAME)
 public interface FullAccessRole {
     String ROLE_NAME = "system-full-access";
 

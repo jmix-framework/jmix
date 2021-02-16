beae638d0f5e5c6afd24234f9114f59bbb0aecde
Maria Orlova <orlova@haulmont.com>
2021-02-16 11:56:40 +0400

    'Cannot get unfetched attribute' exception in ReportEmailTemplate editor Haulmont/jmix-emailtemplates#3

--- emailtemplates-ui/src/main/java/io/jmix/emailtemplatesui/screen/emailtemplate/attachment/EmailTemplateAttachmentsFragment.java
+++ emailtemplates-ui/src/main/java/io/jmix/emailtemplatesui/screen/emailtemplate/attachment/EmailTemplateAttachmentsFragment.java
@@ -25,25 +25,23 @@ import io.jmix.emailtemplates.entity.EmailTemplateAttachment;
 import io.jmix.emailtemplates.entity.TemplateReport;
 import io.jmix.emailtemplatesui.screen.emailtemplate.parameters.EmailTemplateParametersFragment;
 import io.jmix.reports.entity.Report;
-import io.jmix.reportsui.screen.report.browse.ReportBrowser;
 import io.jmix.ui.Actions;
 import io.jmix.ui.Fragments;
+import io.jmix.ui.ScreenBuilders;
 import io.jmix.ui.action.Action;
 import io.jmix.ui.action.ItemTrackingAction;
-import io.jmix.ui.action.list.AddAction;
 import io.jmix.ui.component.Form;
 import io.jmix.ui.component.Table;
 import io.jmix.ui.component.VBoxLayout;
 import io.jmix.ui.download.Downloader;
 import io.jmix.ui.download.FileDataProvider;
-import io.jmix.ui.model.DataContext;
+import io.jmix.ui.model.CollectionPropertyContainer;
 import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @UiController("emailtemplates_EmailTemplateAttachmentsFragment")
@@ -82,19 +80,16 @@ public class EmailTemplateAttachmentsFragment extends ScreenFragment {
     protected Actions actions;
 
     @Autowired
-    protected DataContext dataContext;
+    private ScreenBuilders screenBuilders;
 
     @Autowired
-    @Qualifier("reportsTable.add")
-    protected AddAction addTemplateReportAction;
+    private CollectionPropertyContainer<TemplateReport> attachedReportsDc;
 
     @Subscribe
     public void onAttachEvent(AttachEvent e) {
         Action downloadAction = actions.create(ItemTrackingAction.class, "download")
                 .withHandler(event -> downloadAttachment());
         filesTable.addAction(downloadAction);
-
-        addTemplateReportAction.setScreenClass(ReportBrowser.class);
 
         parametersFragment = fragments.create(this, EmailTemplateParametersFragment.class)
                 .setHideReportCaption(true)
@@ -116,24 +111,33 @@ public class EmailTemplateAttachmentsFragment extends ScreenFragment {
         }
     }
 
-    @Install(to = "reportsTable.add", subject = "transformation")
-    protected Collection<TemplateReport> reportsTableAddTransformation(Collection<Report> entities) {
-        return entities.stream()
-                .map(this::transformToTemplateReport)
-                .collect(Collectors.toList());
+    @Subscribe("reportsTable.add")
+    protected void onReportsTableAdd(Action.ActionPerformedEvent event) {
+        screenBuilders.lookup(Report.class, this)
+                .withSelectHandler(reports -> {
+                    List<TemplateReport> attachedReports =  reports.stream()
+                            .map(this::createTemplateReport)
+                            .collect(Collectors.toList());
+                    attachedReportsDc.getMutableItems().addAll(attachedReports);
+                })
+                .build()
+                .show();
     }
 
-    protected TemplateReport transformToTemplateReport(Report o) {
-        Report report = o;
-        report = dataManager.load(Report.class)
-                .id(report.getId())
-                .fetchPlan("emailTemplate-fetchPlan")
-                .one();
-        TemplateReport templateReport = dataContext.create(TemplateReport.class);
-        templateReport.setReport(report);
+    protected TemplateReport createTemplateReport(Report report) {
+        Report reloaded = reloadReport(report);
+        TemplateReport templateReport = dataManager.create(TemplateReport.class);
+        templateReport.setReport(reloaded);
         templateReport.setEmailTemplate(emailTemplateDc.getItem());
         templateReport.setParameterValues(new ArrayList<>());
         return templateReport;
+    }
+
+    private Report reloadReport(Report report) {
+        return dataManager.load(Report.class)
+                .id(report.getId())
+                .fetchPlan("emailTemplate-fetchPlan")
+                .one();
     }
 
     protected void downloadAttachment() {

cc56d3ae6c3dfbb5214f7d204be855e948032bc5
Roman Pinyazhin <pinyazhin@haulmont.com>
2021-03-05 10:07:43 +0400

    SimplePagination in table/dataGrid should not move navigation buttons to the next line when it is shrank Haulmont/jmix-ui#280

--- ui/src/main/java/io/jmix/ui/widget/JmixSimplePagination.java
+++ ui/src/main/java/io/jmix/ui/widget/JmixSimplePagination.java
@@ -17,6 +17,7 @@
 package io.jmix.ui.widget;
 
 import com.vaadin.ui.Button;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.themes.ValoTheme;
 import org.apache.commons.lang3.StringUtils;
@@ -25,9 +26,12 @@ public class JmixSimplePagination extends JmixAbstractPagination {
 
     public static final String PRIMARY_STYLENAME = "jmix-simplepagination";
 
+    public static final String NAVIGATION_LAYOUT_STYLENAME = PRIMARY_STYLENAME + "-navigation-layout";
     public static final String PAGINATION_STATUS_STYLENAME = PRIMARY_STYLENAME + "-status";
     public static final String PAGINATION_COUNT_STYLENAME = PRIMARY_STYLENAME + "-count";
     public static final String PAGINATION_COUNT_NUMBER_STYLENAME = PRIMARY_STYLENAME + "-count-number";
+
+    protected CssLayout navigationLayout;
 
     protected Label label;
     protected Button countButton;
@@ -42,24 +46,33 @@ public class JmixSimplePagination extends JmixAbstractPagination {
     protected void createContentLayout() {
         setStyleName(PRIMARY_STYLENAME);
 
-        addComponents(getFirstButton(), getPrevButton());
+        navigationLayout = createNavigationLayout();
+        navigationLayout.setStyleName(NAVIGATION_LAYOUT_STYLENAME);
+
+        navigationLayout.addComponents(getFirstButton(), getPrevButton());
 
         label = new Label();
         label.setWidthUndefined();
         label.setStyleName(PAGINATION_STATUS_STYLENAME);
-        addComponent(label);
+        navigationLayout.addComponent(label);
 
         countButton = new Button();
         countButton.setWidthUndefined();
         countButton.setStyleName(ValoTheme.BUTTON_LINK);
         countButton.addStyleName(PAGINATION_COUNT_STYLENAME);
         countButton.setTabIndex(-1);
-        addComponent(countButton);
+        navigationLayout.addComponent(countButton);
 
-        addComponents(getNextButton(), getLastButton());
+        navigationLayout.addComponents(getNextButton(), getLastButton());
+
+        addComponent(navigationLayout);
 
         getItemsPerPageLayout().setVisible(false);
         addComponent(getItemsPerPageLayout());
+    }
+
+    protected CssLayout createNavigationLayout() {
+        return new CssLayout();
     }
 
     public Label getLabel() {

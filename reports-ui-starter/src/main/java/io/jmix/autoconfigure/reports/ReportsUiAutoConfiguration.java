872549b74657fad159dc9fdc032d6502f01a982d
Dmitry Cherkasov <43281522+KartonDCP@users.noreply.github.com>
2021-03-12 12:17:07 +0300

    Moved Import(UiExportConfiguration.class) (#64)
    from ReportsUIConfiguration to ReportsUiAutoConfiguration #34

--- reports-ui-starter/src/main/java/io/jmix/autoconfigure/reports/ReportsUiAutoConfiguration.java
+++ reports-ui-starter/src/main/java/io/jmix/autoconfigure/reports/ReportsUiAutoConfiguration.java
@@ -4,10 +4,11 @@ import io.jmix.core.CoreConfiguration;
 import io.jmix.data.DataConfiguration;
 import io.jmix.reports.ReportsConfiguration;
 import io.jmix.reportsui.ReportsUIConfiguration;
+import io.jmix.uiexport.UiExportConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@Import({CoreConfiguration.class, DataConfiguration.class, ReportsConfiguration.class, ReportsUIConfiguration.class})
+@Import({CoreConfiguration.class, DataConfiguration.class, ReportsConfiguration.class, ReportsUIConfiguration.class, UiExportConfiguration.class})
 public class ReportsUiAutoConfiguration {
 }
--- reports-ui/src/main/java/io/jmix/reportsui/ReportsUIConfiguration.java
+++ reports-ui/src/main/java/io/jmix/reportsui/ReportsUIConfiguration.java
@@ -9,21 +9,18 @@ import io.jmix.reportsui.screen.definition.edit.crosstab.CrossTabTableDecorator;
 import io.jmix.ui.UiConfiguration;
 import io.jmix.ui.sys.ActionsConfiguration;
 import io.jmix.ui.sys.UiControllersConfiguration;
-import io.jmix.uiexport.UiExportConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Import;
 
 import java.util.Collections;
 
 @Configuration
 @ComponentScan
 @ConfigurationPropertiesScan
-@Import(UiExportConfiguration.class)
 @JmixModule(dependsOn = {CoreConfiguration.class, UiConfiguration.class, ReportsConfiguration.class})
 @PropertySource(name = "io.jmix.reportsui", value = "classpath:/io/jmix/reportsui/module.properties")
 public class ReportsUIConfiguration {

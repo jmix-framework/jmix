0678364da5b433b9153a92aea3f78ecac12a3169
subbotin <subbotin@haulmont.com>
2021-03-15 22:08:24 +0400

    Extract REST Oauth security into separate reusable module Haulmont/jmix-security#59

---
+++ core/src/main/java/io/jmix/core/security/AuthorizedUrlsProvider.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.core.security;
+
+import java.util.Collection;
+
+/**
+ * Provider used when configure http web security for API endpoints (like REST, MVC controllers)
+ * Specifies lists of authenticated and anonymous URLs.
+ */
+public interface AuthorizedUrlsProvider {
+    /**
+     * Specify that URL patterns are allowed by any authenticated user.
+     */
+    Collection<String> getAuthenticatedUrlPatterns();
+
+    /**
+     * Specify that URL patterns are allowed by anonymous user.
+     */
+    Collection<String> getAnonymousUrlPatterns();
+}

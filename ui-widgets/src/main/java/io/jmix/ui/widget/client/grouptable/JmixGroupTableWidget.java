21a08594c2dbf9740c2093062711d9c8fbd63d2f
Gleb Durygin <durygin@haulmont.com>
2021-03-18 13:42:33 +0400

    Need to generate j-test-id for aggregated textFields Haulmont/jmix-ui#160

--- ui-widgets/src/main/java/io/jmix/ui/widget/client/grouptable/JmixGroupTableWidget.java
+++ ui-widgets/src/main/java/io/jmix/ui/widget/client/grouptable/JmixGroupTableWidget.java
@@ -22,7 +22,6 @@ import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Event;
 import com.google.gwt.user.client.ui.Widget;
-import io.jmix.ui.AppUI;
 import io.jmix.ui.widget.client.Tools;
 import io.jmix.ui.widget.client.aggregation.AggregationInputFieldInfo;
 import io.jmix.ui.widget.client.aggregation.TableAggregationRow;
@@ -790,15 +789,17 @@ public class JmixGroupTableWidget extends JmixScrollTableWidget {
                 inputElement.setValue(text);
                 inputElement.addClassName("v-textfield v-widget");
                 inputElement.addClassName("c-group-aggregation-textfield");
-
-                AppUI ui = AppUI.getCurrent();
-                if (ui != null && ui.isTestMode()) {
-                    inputElement.setAttribute("j-test-id", "aggregationField");
-                }
-
                 Style elemStyle = inputElement.getStyle();
                 elemStyle.setWidth(100, Style.Unit.PCT);
                 DOM.appendChild(container, inputElement);
+
+                HeaderCell headerCell = getHead().getHeaderCell(colIndex);
+                if (headerCell != null) {
+                    String headerCellJTEstId = headerCell.getElement().getAttribute("j-test-id");
+                    if (headerCellJTEstId != null && !headerCellJTEstId.isEmpty()) {
+                        inputElement.setAttribute("j-test-id", "aggregationField");
+                    }
+                }
 
                 if (inputsList == null) {
                     inputsList = new ArrayList<>();

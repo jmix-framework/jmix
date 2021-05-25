aa925b1051015b611654e163674137fd2796575e
Maria Orlova <orlova@haulmont.com>
2021-05-25 21:18:04 +0400

    Fix getIdFromString method Haulmont/jmix-reports#106

--- reports-rest/src/main/java/io/jmix/reportsrest/controller/ReportRestControllerManager.java
+++ reports-rest/src/main/java/io/jmix/reportsrest/controller/ReportRestControllerManager.java
@@ -322,36 +322,6 @@ public class ReportRestControllerManager {
 
     protected Object getIdFromString(String entityId, MetaClass metaClass) {
         try {
-            //TODO get ID
-//            if (BaseDbGeneratedIdEntity.class.isAssignableFrom(metaClass.getJavaClass())) {
-//                if (BaseIdentityIdEntity.class.isAssignableFrom(metaClass.getJavaClass())) {
-//                    return IdProxy.of(Long.valueOf(entityId));
-//                } else if (BaseIntIdentityIdEntity.class.isAssignableFrom(metaClass.getJavaClass())) {
-//                    return IdProxy.of(Integer.valueOf(entityId));
-//                } else {
-            Class<?> clazz = metaClass.getJavaClass();
-            while (clazz != null) {
-                Method[] methods = clazz.getDeclaredMethods();
-                for (Method method : methods) {
-                    if (method.getName().equals("getDbGeneratedId")) {
-                        Class<?> idClass = method.getReturnType();
-                        if (Long.class.isAssignableFrom(idClass)) {
-                            return Long.valueOf(entityId);
-                        } else if (Integer.class.isAssignableFrom(idClass)) {
-                            return Integer.valueOf(entityId);
-                        } else if (Short.class.isAssignableFrom(idClass)) {
-                            return Long.valueOf(entityId);
-                        } else if (UUID.class.isAssignableFrom(idClass)) {
-                            return UUID.fromString(entityId);
-                        }
-                    }
-                }
-                clazz = clazz.getSuperclass();
-//                    }
-            }
-//                throw new UnsupportedOperationException("Unsupported ID type in entity " + metaClass.getName());
-//            } else {
-            //noinspection unchecked
             Method getIdMethod = metaClass.getJavaClass().getMethod("getId");
             Class<?> idClass = getIdMethod.getReturnType();
             if (UUID.class.isAssignableFrom(idClass)) {
@@ -363,7 +333,6 @@ public class ReportRestControllerManager {
             } else {
                 return entityId;
             }
-//            }
         } catch (Exception e) {
             throw new RestAPIException("Invalid entity ID",
                     String.format("Cannot convert %s into valid entity ID", entityId),

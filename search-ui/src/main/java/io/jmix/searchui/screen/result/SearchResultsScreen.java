a17d82ed4a832361344a51a888a250dd430cf0b8
subbotin <a.subbotin@haulmont.com>
2021-05-28 13:17:17 +0400

    Rename UnsafeDataManager to UnconstrainedDataManager Haulmont/jmix-core#169

--- search-ui/src/main/java/io/jmix/searchui/screen/result/SearchResultsScreen.java
+++ search-ui/src/main/java/io/jmix/searchui/screen/result/SearchResultsScreen.java
@@ -67,7 +67,7 @@ public class SearchResultsScreen extends Screen {
     @Autowired
     protected ScreenBuilders screenBuilders;
     @Autowired
-    protected UnsafeDataManager dataManager;
+    protected UnconstrainedDataManager dataManager;
     @Autowired
     protected SearchApplicationProperties searchApplicationProperties;
     @Autowired

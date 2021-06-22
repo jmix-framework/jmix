2f7a26bfdc0e9ac14a3df5e5406c25a4c16b8379
subbotin <a.subbotin@haulmont.com>
2021-06-22 21:15:00 +0400

    Port ability to use @Transient with @Temporal Haulmont/jmix-core#194

--- src/main/groovy/io/jmix/gradle/JmixPlugin.groovy
+++ src/main/groovy/io/jmix/gradle/JmixPlugin.groovy
@@ -66,7 +66,7 @@ class JmixPlugin implements Plugin<Project> {
 
             if (project.jmix.entitiesEnhancing.enabled) {
                 project.configurations.create('enhancing')
-                project.dependencies.add('enhancing', 'org.eclipse.persistence:org.eclipse.persistence.jpa:2.7.7-2-jmix')
+                project.dependencies.add('enhancing', 'org.eclipse.persistence:org.eclipse.persistence.jpa:2.7.7-4-jmix')
 
                 project.tasks.findByName('compileJava').doLast(new EnhancingAction('main'))
                 project.tasks.findByName('classes').doLast({ EnhancingAction.copyGeneratedFiles(project, 'main') })

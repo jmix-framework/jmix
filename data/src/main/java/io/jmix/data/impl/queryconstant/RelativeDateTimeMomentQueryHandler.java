0308e8cfaa0a13b8c4043172d254f1b33d69f7a0
Vladislav Plakhov <v.plakhov@haulmont.com>
2021-06-25 12:00:55 +0400

    CURRENT_HOUR/CURRENT_MINUTE relative date time processed incorrect #80

--- data/src/main/java/io/jmix/data/impl/queryconstant/RelativeDateTimeMomentQueryHandler.java
+++ data/src/main/java/io/jmix/data/impl/queryconstant/RelativeDateTimeMomentQueryHandler.java
@@ -20,6 +20,7 @@ import io.jmix.core.TimeSource;
 import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.data.DataProperties;
 import io.jmix.data.impl.QueryConstantHandler;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -62,56 +63,64 @@ public class RelativeDateTimeMomentQueryHandler implements QueryConstantHandler 
     private String getValueForConstant(RelativeDateTimeMoment moment) {
         String result = "'%s'";
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-        LocalDate now = LocalDate.now();
+        ZoneId userTimeZone = getUserZoneId();
+        LocalDate nowDate = LocalDate.now(userTimeZone);
+        LocalDateTime nowDateTime = LocalDateTime.now(userTimeZone);
         Locale locale = Locale.getDefault();
         if (dataProperties.isUseUserLocaleForRelativeDateTimeMoments()) {
             if (currentAuthentication.isSet()) {
                 locale = currentAuthentication.getLocale();
             }
         }
-        int currentYear = LocalDate.now().getYear();
+        int currentYear = nowDate.getYear();
         switch (moment) {
             case FIRST_DAY_OF_CURRENT_YEAR:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now.withDayOfYear(1), LocalTime.MIDNIGHT)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate.withDayOfYear(1), LocalTime.MIDNIGHT)).format(dateTimeFormatter));
             case LAST_DAY_OF_CURRENT_YEAR:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of((now.withDayOfYear(now.lengthOfYear())), LocalTime.MAX)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of((nowDate.withDayOfYear(nowDate.lengthOfYear())), LocalTime.MAX)).format(dateTimeFormatter));
             case FIRST_DAY_OF_CURRENT_MONTH:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(LocalDate.of(currentYear, now.getMonth(), 1), LocalTime.MIDNIGHT)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(LocalDate.of(currentYear, nowDate.getMonth(), 1), LocalTime.MIDNIGHT)).format(dateTimeFormatter));
             case LAST_DAY_OF_CURRENT_MONTH:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(LocalDate.of(currentYear, now.getMonth(), now.getMonth().length(now.isLeapYear())), LocalTime.MAX)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(LocalDate.of(currentYear, nowDate.getMonth(), nowDate.getMonth().length(nowDate.isLeapYear())), LocalTime.MAX)).format(dateTimeFormatter));
             case FIRST_DAY_OF_CURRENT_WEEK:
                 TemporalField fieldISO = WeekFields.of(locale).dayOfWeek();
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now.with(fieldISO, 1), LocalTime.MIDNIGHT)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate.with(fieldISO, 1), LocalTime.MIDNIGHT)).format(dateTimeFormatter));
             case LAST_DAY_OF_CURRENT_WEEK:
                 fieldISO = WeekFields.of(locale).dayOfWeek();
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now.with(fieldISO, 7), LocalTime.MAX)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate.with(fieldISO, 7), LocalTime.MAX)).format(dateTimeFormatter));
             case START_OF_CURRENT_DAY:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now, LocalTime.MIDNIGHT)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate, LocalTime.MIDNIGHT)).format(dateTimeFormatter));
             case END_OF_CURRENT_DAY:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now, LocalTime.MAX)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate, LocalTime.MAX)).format(dateTimeFormatter));
             case START_OF_YESTERDAY:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now, LocalTime.MIDNIGHT).minusDays(1)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate, LocalTime.MIDNIGHT).minusDays(1)).format(dateTimeFormatter));
             case START_OF_TOMORROW:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(now, LocalTime.MIDNIGHT).plusDays(1)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(LocalDateTime.of(nowDate, LocalTime.MIDNIGHT).plusDays(1)).format(dateTimeFormatter));
             case START_OF_CURRENT_HOUR:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.now().withMinute(0).withSecond(0).withNano(0)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(nowDateTime.withMinute(0).withSecond(0).withNano(0)).format(dateTimeFormatter));
             case END_OF_CURRENT_HOUR:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.now().withMinute(59).withSecond(59).withNano(999_999_999)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(nowDateTime.withMinute(59).withSecond(59).withNano(999_999_999)).format(dateTimeFormatter));
             case START_OF_CURRENT_MINUTE:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.now().withSecond(0).withNano(0)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(nowDateTime.withSecond(0).withNano(0)).format(dateTimeFormatter));
             case END_OF_CURRENT_MINUTE:
-                return String.format(result, convertToApplicationTimeZone(LocalDateTime.now().withSecond(59).withNano(999_999_999)).format(dateTimeFormatter));
+                return String.format(result, convertToApplicationTimeZone(nowDateTime.withSecond(59).withNano(999_999_999)).format(dateTimeFormatter));
         }
         return moment.name();
     }
 
     private LocalDateTime convertToApplicationTimeZone(LocalDateTime localDateTime) {
         ZoneId applicationZoneId = timeSource.now().getZone();
+        ZoneId userTimeZone = getUserZoneId();
+        return ZonedDateTime.of(localDateTime, userTimeZone).withZoneSameInstant(applicationZoneId).toLocalDateTime();
+    }
+
+    @NotNull
+    private ZoneId getUserZoneId() {
         ZoneId userTimeZone = ZoneId.systemDefault();
         if (currentAuthentication.isSet()) {
             userTimeZone = ZoneId.of(currentAuthentication.getTimeZone().getID());
         }
-        return ZonedDateTime.of(localDateTime, userTimeZone).withZoneSameInstant(applicationZoneId).toLocalDateTime();
+        return userTimeZone;
     }
 
 }

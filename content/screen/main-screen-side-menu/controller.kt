c8cc861b4b417264d613572c191360ad2a6b853c
Timur Musin <musintd@haulmont.com>
2021-07-19 13:09:51 +0400

    Add main screen with side menu template based on Drawer Haulmont/jmix-templates#14

--- content/screen/main-screen-side-menu/controller.kt
+++ content/screen/main-screen-side-menu/controller.kt
@@ -1,11 +1,53 @@
 package ${packageName}
 
-import com.haulmont.cuba.gui.screen.UiController
-import com.haulmont.cuba.gui.screen.UiDescriptor
-import com.haulmont.cuba.web.app.main.MainScreen
-<%if (classComment) {%>
-${classComment}<%}%>
+import io.jmix.ui.ScreenTools
+import io.jmix.ui.component.AppWorkArea
+import io.jmix.ui.component.Button
+import io.jmix.ui.component.Window.HasWorkArea
+import io.jmix.ui.component.mainwindow.Drawer
+import io.jmix.ui.icon.JmixIcon
+import io.jmix.ui.navigation.Route
+import io.jmix.ui.screen.*
+import org.springframework.beans.factory.annotation.Autowired
 
-@UiController("${api.escapeKotlinDollar(id)}")
+@UiController("${id}")
 @UiDescriptor("${descriptorName}.xml")
-class ${controllerName} : MainScreen()
+@Route(path = "main", root = true)
+open class ${controllerName} : Screen(), HasWorkArea {
+
+    @Autowired
+    private lateinit var screenTools: ScreenTools
+
+    @Autowired
+    private lateinit var workArea: AppWorkArea
+
+    @Autowired
+    private lateinit var drawer: Drawer
+
+    @Autowired
+    private lateinit var collapseDrawerButton: Button
+
+    override fun getWorkArea(): AppWorkArea = workArea
+
+    @Subscribe("collapseDrawerButton")
+    private fun onCollapseDrawerButtonClick(event: Button.ClickEvent) {
+        drawer.run {
+            toggle()
+            if (isCollapsed) {
+                collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_RIGHT)
+            } else {
+                collapseDrawerButton.setIconFromSet(JmixIcon.CHEVRON_LEFT)
+            }
+        }
+    }
+
+    @Subscribe
+    fun onAfterShow(event: AfterShowEvent?) {
+        screenTools.run {
+            openDefaultScreen(
+                UiControllerUtils.getScreenContext(this@${controllerName}).screens
+            )
+            handleRedirect()
+        }
+    }
+}

6a197aafcc15f8076468eb742ac64aba3bba86d8
Sergey Gurilev <GyrSergey@gmail.com>
2021-08-31 10:42:05 +0400

    Refactoring common part of Rest and Graphql modules (#81)
    Refactoring common part of Rest and Graphql modules

--- rest/src/main/java/io/jmix/rest/impl/controller/FileDownloadController.java
+++ rest/src/main/java/io/jmix/rest/impl/controller/FileDownloadController.java
@@ -16,14 +16,12 @@
 
 package io.jmix.rest.impl.controller;
 
-import io.jmix.core.*;
-import io.jmix.core.common.util.URLEncodeUtils;
+import io.jmix.core.AccessManager;
+import io.jmix.core.FileClientManager;
+import io.jmix.core.FileRef;
+import io.jmix.core.Metadata;
 import io.jmix.rest.accesscontext.RestFileDownloadContext;
 import io.jmix.rest.exception.RestAPIException;
-import org.apache.commons.io.FilenameUtils;
-import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.BooleanUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,10 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * REST API controller that is used for downloading files
@@ -48,11 +43,11 @@ public class FileDownloadController {
     private static final Logger log = LoggerFactory.getLogger(FileDownloadController.class);
 
     @Autowired
-    protected FileStorageLocator fileStorageLocator;
-    @Autowired
     protected Metadata metadata;
     @Autowired
     protected AccessManager accessManager;
+    @Autowired
+    FileClientManager fileClientManager;
 
     @GetMapping
     public void downloadFile(@RequestParam String fileRef,
@@ -60,72 +55,16 @@ public class FileDownloadController {
                              HttpServletResponse response) {
 
         checkFileDownloadPermission();
-        FileRef fileReference;
         try {
+            FileRef fileReference;
             fileReference = FileRef.fromString(fileRef);
+            fileClientManager.downloadAndWriteResponse(fileReference, fileReference.getStorageName(), attachment, response);
         } catch (IllegalArgumentException e) {
             throw new RestAPIException("Invalid file reference",
                     String.format("Cannot convert '%s' into valid file reference", fileRef),
                     HttpStatus.BAD_REQUEST,
                     e);
         }
-
-        FileStorage fileStorage;
-        try {
-            fileStorage = fileStorageLocator.getByName(fileReference.getStorageName());
-        } catch (IllegalArgumentException e) {
-            throw new RestAPIException("Invalid file reference",
-                    String.format("Cannot find FileStorage for the given FileRef: '%s'", fileRef),
-                    HttpStatus.BAD_REQUEST,
-                    e);
-        }
-
-        //check if a file by the given reference exists
-        if (!fileStorage.fileExists(fileReference)) {
-            throw new RestAPIException("File not found", "File not found. File reference: " +
-                    fileRef, HttpStatus.NOT_FOUND);
-        }
-
-        try {
-            response.setHeader("Cache-Control", "no-cache");
-            response.setHeader("Pragma", "no-cache");
-            response.setDateHeader("Expires", 0);
-            response.setHeader("Content-Type", getContentType(fileReference));
-
-            String filename = fileReference.getFileName();
-            String contentDisposition = BooleanUtils.isTrue(attachment) ? "attachment" : "inline";
-            if (StringUtils.isNotEmpty(filename)) {
-                contentDisposition += "; filename=\"" + URLEncodeUtils.encodeUtf8(filename) + "\"";
-            }
-            response.setHeader("Content-Disposition", contentDisposition);
-
-            downloadAndWriteResponse(fileStorage, fileReference, response);
-        } catch (Exception e) {
-            log.error("Error on downloading the file {}", fileRef, e);
-            throw new RestAPIException("Error on downloading the file", "", HttpStatus.INTERNAL_SERVER_ERROR, e);
-        }
-    }
-
-    protected void downloadAndWriteResponse(FileStorage fileStorage, FileRef fileRef,
-                                            HttpServletResponse response) throws IOException {
-        ServletOutputStream os = response.getOutputStream();
-        try (InputStream is = fileStorage.openStream(fileRef)) {
-            IOUtils.copy(is, os);
-            os.flush();
-        } catch (FileStorageException e) {
-            throw new RestAPIException("Unable to download file from FileStorage",
-                    "Unable to download file from FileStorage: " + fileRef.toString(),
-                    HttpStatus.INTERNAL_SERVER_ERROR,
-                    e);
-        }
-    }
-
-    protected String getContentType(FileRef fileRef) {
-        String extension = FilenameUtils.getExtension(fileRef.getFileName());
-        if (StringUtils.isEmpty(extension)) {
-            return FileTypesHelper.DEFAULT_MIME_TYPE;
-        }
-        return FileTypesHelper.getMIMEType("." + extension.toLowerCase());
     }
 
     protected void checkFileDownloadPermission() {

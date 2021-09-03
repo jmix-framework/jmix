4af04f4e92b24fe585fb8f99adc131cddac8c9f9
Konstantin Krivopustov <krivopustov@haulmont.com>
2021-09-03 11:11:56 +0400

    Fix template Haulmont/jmix-security#97

--- content/project/single-module-application/src/main/java/${project_rootPath}/screen/user/UserEdit.java
+++ content/project/single-module-application/src/main/java/${project_rootPath}/screen/user/UserEdit.java
@@ -3,6 +3,7 @@ package ${project_rootPackage}.screen.user;
 import ${project_rootPackage}.entity.User;
 import io.jmix.core.EntityStates;
 import io.jmix.ui.Notifications;
+import io.jmix.ui.component.ComboBox;
 import io.jmix.ui.component.PasswordField;
 import io.jmix.ui.component.TextField;
 import io.jmix.ui.navigation.Route;
@@ -10,7 +11,9 @@ import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Arrays;
 import java.util.Objects;
+import java.util.TimeZone;
 
 @UiController("${normalizedPrefix_underscore}User.edit")
 @UiDescriptor("user-edit.xml")

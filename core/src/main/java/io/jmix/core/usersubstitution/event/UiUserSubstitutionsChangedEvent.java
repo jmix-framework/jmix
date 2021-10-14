2b57f069e853a768df8033537e52ff0aeea68af6
Gleb Gorelov <g.gorelov@haulmont.com>
2021-10-14 15:04:01 +0400

    Update UserIndicator appearance when user substitutions are added/removed #678

---
+++ core/src/main/java/io/jmix/core/usersubstitution/event/UiUserSubstitutionsChangedEvent.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2021 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.core.usersubstitution.event;
+
+import org.springframework.context.ApplicationEvent;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Event that is fired for UI when UserDetails substitutions for currently logged in user are changed.
+ */
+public class UiUserSubstitutionsChangedEvent extends ApplicationEvent {
+
+    public UiUserSubstitutionsChangedEvent(UserDetails source) {
+        super(source);
+    }
+
+    @Override
+    public UserDetails getSource() {
+        return (UserDetails) super.getSource();
+    }
+}

84c6c161f6c1f312ad9355b8459d9d79f6bd4e06
Maxim Gorbunkov <gorbunkov@haulmont.com>
2022-01-25 17:19:26 +0400

    Comment unused TokenStore injection in UserSessionControllerManager
    This caused problems when jmix-rest dependency is added to the project, but security-oauth2 is not.

--- rest/src/main/java/io/jmix/rest/impl/service/UserSessionControllerManager.java
+++ rest/src/main/java/io/jmix/rest/impl/service/UserSessionControllerManager.java
@@ -25,7 +25,7 @@ import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.stereotype.Component;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,8 +37,8 @@ public class UserSessionControllerManager {
 
     @Autowired
     protected RestLocaleUtils restLocaleUtils;
-    @Autowired
-    protected TokenStore tokenStore;
+//    @Autowired
+//    protected TokenStore tokenStore;
 
     public void setSessionLocale(HttpServletRequest request) {
         Locale locale = restLocaleUtils.extractLocaleFromRequestHeader(request);

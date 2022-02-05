0b43afca07b8106346e7c1ebbaebb4dfe7fb865e
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-02-05 14:58:27 +0400

    REST API-only application template #51

---
+++ content/project/rest-service/src/main/java/${project_rootPath}/${project_classPrefix}Application.java
@@ -0,0 +1,49 @@
+package ${project_rootPackage};
+
+import com.google.common.base.Strings;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class ${project_classPrefix}Application {
+
+    @Autowired
+    private Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(${project_classPrefix}Application.class, args);
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource")
+    DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    @ConfigurationProperties("main.datasource.hikari")
+    DataSource dataSource(DataSourceProperties dataSourceProperties) {
+        return dataSourceProperties.initializeDataSourceBuilder().build();
+    }
+
+    @EventListener
+    public void printApplicationUrl(ApplicationStartedEvent event) {
+        LoggerFactory.getLogger(${project_classPrefix}Application.class).info("Application started at "
+                + "http://localhost:"
+                + environment.getProperty("local.server.port")
+                + Strings.nullToEmpty(environment.getProperty("server.servlet.context-path")));
+    }
+}
---
+++ content/project/rest-service/src/main/java/${project_rootPath}/security/FullAccessRole.java
@@ -0,0 +1,19 @@
+package ${project_rootPackage}.security;
+
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
+import io.jmix.security.role.annotation.ResourceRole;
+import io.jmix.security.role.annotation.SpecificPolicy;
+
+@ResourceRole(name = "Full Access", code = FullAccessRole.CODE)
+public interface FullAccessRole {
+
+    String CODE = "system-full-access";
+
+    @EntityPolicy(entityName = "*", actions = {EntityPolicyAction.ALL})
+    @EntityAttributePolicy(entityName = "*", attributes = "*", action = EntityAttributePolicyAction.MODIFY)
+    @SpecificPolicy(resources = "*")
+    void fullAccess();
+}

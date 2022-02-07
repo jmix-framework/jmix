e0dc72bc6da7c5f5062937fdfa6e57979039af69
Maxim Gorbunkov <gorbunkov@haulmont.com>
2022-02-07 20:53:33 +0400

    Tests fixed

--- oidc/src/test/java/io/jmix/oidc/OidcTest.java
+++ oidc/src/test/java/io/jmix/oidc/OidcTest.java
@@ -1,33 +1,12 @@
 package io.jmix.oidc;
 
-import io.jmix.oidc.entity.Foo;
-import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class OidcTest {
 
-	@Autowired
-	DataManager dataManager;
-
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void testFoo() {
-		Foo foo = dataManager.create(Foo.class);
-		foo.setName("abc");
-
-		Foo foo1 = dataManager.save(foo);
-		assertEquals(foo, foo1);
-
-		Foo foo2 = dataManager.load(Id.of(foo)).one();
-		assertEquals(foo, foo2);
 	}
 }

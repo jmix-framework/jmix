8b8be70c2b08eb3473868cb8c4b44506f4be1bb5
Maxim Gorbunkov <gorbunkov@haulmont.com>
2022-02-07 20:54:46 +0400

    Tests fixed

--- saml/src/test/java/io/jmix/saml/SamlTest.java
+++ saml/src/test/java/io/jmix/saml/SamlTest.java
@@ -1,33 +1,12 @@
 package io.jmix.saml;
 
-import io.jmix.saml.entity.Foo;
-import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SamlTest {
 
-	@Autowired
-	DataManager dataManager;
-
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void testFoo() {
-		Foo foo = dataManager.create(Foo.class);
-		foo.setName("abc");
-
-		Foo foo1 = dataManager.save(foo);
-		assertEquals(foo, foo1);
-
-		Foo foo2 = dataManager.load(Id.of(foo)).one();
-		assertEquals(foo, foo2);
 	}
 }
--- saml/src/test/java/io/jmix/saml/SamlTestConfiguration.java
+++ saml/src/test/java/io/jmix/saml/SamlTestConfiguration.java
@@ -3,26 +3,11 @@ package io.jmix.saml;
 import io.jmix.core.annotation.JmixModule;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-
-import javax.sql.DataSource;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @Import(SamlConfiguration.class)
 @JmixModule(id = "io.jmix.saml.test", dependsOn = SamlConfiguration.class)
 public class SamlTestConfiguration {
-
-    @Bean
-    @Primary
-    DataSource dataSource() {
-        return new EmbeddedDatabaseBuilder()
-                .generateUniqueName(true)
-                .setType(EmbeddedDatabaseType.HSQL)
-                .build();
-    }
 }

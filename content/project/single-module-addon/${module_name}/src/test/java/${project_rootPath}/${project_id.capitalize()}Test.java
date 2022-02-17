1653551ae4506c0e58c55c1dacef5bd59223ff63
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-02-17 16:41:47 +0400

    Remove sample user entity and screens from Single-Module add-on template #70

--- content/project/single-module-addon/${module_name}/src/test/java/${project_rootPath}/${project_id.capitalize()}Test.java
+++ content/project/single-module-addon/${module_name}/src/test/java/${project_rootPath}/${project_id.capitalize()}Test.java
@@ -1,10 +1,6 @@
 package ${project_rootPackage};
 
-import ${project_rootPackage}.entity.Foo;
-import io.jmix.core.DataManager;
-import io.jmix.core.Id;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,22 +8,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class ${project_id.capitalize()}Test {
 
-	@Autowired
-	DataManager dataManager;
-
 	@Test
 	void contextLoads() {
-	}
-
-	@Test
-	void testFoo() {
-		Foo foo = dataManager.create(Foo.class);
-		foo.setName("abc");
-
-		Foo foo1 = dataManager.save(foo);
-		assertEquals(foo, foo1);
-
-		Foo foo2 = dataManager.load(Id.of(foo)).one();
-		assertEquals(foo, foo2);
 	}
 }

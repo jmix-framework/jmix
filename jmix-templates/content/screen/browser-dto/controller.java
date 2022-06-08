dfbf4e84eafb178d2c197f1de6f1c089524fd392
Konstantin Krivopustov <krivopustov@haulmont.com>
2022-06-08 19:08:42 +0400

    Improve DTO screen templates #740

--- jmix-templates/content/screen/browser-dto/controller.java
+++ jmix-templates/content/screen/browser-dto/controller.java
@@ -1,7 +1,11 @@
 package ${packageName};
 
+import io.jmix.core.LoadContext;
 import io.jmix.ui.screen.*;
 import ${entity.fqn};
+
+import java.util.Collections;
+import java.util.List;
 
 <%if (classComment) {%>
 ${classComment}
@@ -9,4 +13,10 @@ ${classComment}
 @UiDescriptor("${lookupDescriptorName}.xml")
 @LookupComponent("${tableId}")
 public class ${lookupControllerName} extends StandardLookup<${entity.className}> {
+
+    @Install(to = "${tableDl}", target = Target.DATA_LOADER)
+    private List<${entity.className}> ${tableDl}LoadDelegate(LoadContext<${entity.className}> loadContext) {
+        // Here you can load entities from an external store
+        return Collections.emptyList();
+    }
 }
--- jmix-templates/content/screen/browser-dto/controller.kt
+++ jmix-templates/content/screen/browser-dto/controller.kt
@@ -1,6 +1,8 @@
 package ${packageName}
 
+import io.jmix.core.LoadContext
 import io.jmix.ui.screen.*
+import io.jmix.ui.screen.Target
 import ${entity.fqn}
 
 <%if (classComment) {%>
@@ -8,4 +10,11 @@ ${classComment}
 <%}%>@UiController("${lookupId}")
 @UiDescriptor("${lookupDescriptorName}.xml")
 @LookupComponent("${tableId}")
-class ${lookupControllerName} : StandardLookup<${entity.className}>()
+class ${lookupControllerName} : StandardLookup<${entity.className}>() {
+
+    @Install(to = "${tableDl}", target = Target.DATA_LOADER)
+    private fun ${tableDl}LoadDelegate(loadContext: LoadContext<${entity.className}>?): MutableList<${entity.className}> {
+        // Here you can load entities from an external store
+        return mutableListOf()
+    }
+}

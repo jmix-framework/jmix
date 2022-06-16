e35b674fbe6ec82262ebafeebbc3211190fed119
Artem Artemev <a.artemev@haulmont.com>
2022-06-16 19:08:30 +0400

    Provide possibility to export and import dynamic attributes #378

--- jmix-dynattr/dynattr-ui/src/main/java/io/jmix/dynattrui/screen/category/CategoryBrowse.java
+++ jmix-dynattr/dynattr-ui/src/main/java/io/jmix/dynattrui/screen/category/CategoryBrowse.java
@@ -16,9 +16,11 @@
 
 package io.jmix.dynattrui.screen.category;
 
+import com.google.common.io.Files;
 import io.jmix.core.*;
 import io.jmix.core.accesscontext.CrudEntityContext;
 import io.jmix.core.metamodel.model.MetaClass;
+import io.jmix.data.entity.ReferenceToEntity;
 import io.jmix.dynattr.AttributeType;
 import io.jmix.dynattr.DynAttrMetadata;
 import io.jmix.dynattr.model.Category;
@@ -26,21 +28,33 @@ import io.jmix.dynattr.model.CategoryAttribute;
 import io.jmix.ui.Notifications;
 import io.jmix.ui.UiComponents;
 import io.jmix.ui.action.Action;
-import io.jmix.ui.component.Button;
-import io.jmix.ui.component.Label;
-import io.jmix.ui.component.Table;
+import io.jmix.ui.component.*;
+import io.jmix.ui.download.DownloadFormat;
+import io.jmix.ui.download.Downloader;
 import io.jmix.ui.model.CollectionContainer;
 import io.jmix.ui.model.CollectionLoader;
 import io.jmix.ui.model.InstanceContainer;
 import io.jmix.ui.model.InstanceLoader;
+import io.jmix.ui.screen.LookupComponent;
 import io.jmix.ui.screen.*;
 import org.apache.commons.lang3.BooleanUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import static io.jmix.ui.download.DownloadFormat.JSON;
+import static io.jmix.ui.download.DownloadFormat.ZIP;
 
 @UiController("dynat_Category.browse")
 @UiDescriptor("category-browse.xml")
 @LookupComponent("categoriesTable")
 public class CategoryBrowse extends StandardLookup<Category> {
+
+    private static final Logger log = LoggerFactory.getLogger(CategoryBrowse.class);
 
     @Autowired
     protected Notifications notifications;
@@ -54,7 +68,19 @@ public class CategoryBrowse extends StandardLookup<Category> {
     protected MessageTools messageTools;
     @Autowired
     protected DynAttrMetadata dynAttrMetadata;
+    @Autowired
+    protected EntityImportExport entityImportExport;
+    @Autowired
+    protected FetchPlans fetchPlans;
+    @Autowired
+    protected EntityImportPlans entityImportPlans;
+    @Autowired
+    protected Downloader downloader;
+    @Autowired
+    protected FileUploadField importField;
 
+    @Autowired
+    protected GroupTable<Category> categoriesTable;
     @Autowired
     protected CollectionContainer<CategoryAttribute> attributesDc;
     @Autowired
@@ -137,5 +163,97 @@ public class CategoryBrowse extends StandardLookup<Category> {
         if (!crudEntityContext.isUpdatePermitted()) {
             applyChangesBtn.setEnabled(false);
         }
+    }
+
+    @Subscribe("exportBtn.exportJSON")
+    public void onExportBtnExportJSON(Action.ActionPerformedEvent event) {
+        export(JSON);
+    }
+
+    @Subscribe("exportBtn.exportZIP")
+    public void onExportBtnExportZIP(Action.ActionPerformedEvent event) {
+        export(ZIP);
+    }
+
+    protected void export(DownloadFormat downloadFormat) {
+        Collection<Category> selected = categoriesTable.getSelected();
+        if (selected.isEmpty() && categoriesTable.getItems() != null) {
+            selected = categoriesTable.getItems().getItems();
+        }
+
+        if (selected.isEmpty()) {
+            notifications.create(Notifications.NotificationType.HUMANIZED)
+                    .withDescription(messages.getMessage(CategoryBrowse.class, "nothingToExport"))
+                    .show();
+            return;
+        }
+
+        try {
+            byte[] data;
+            if (downloadFormat == JSON) {
+                data = entityImportExport.exportEntitiesToJSON(new ArrayList<>(selected), buildExportFetchPlan()).getBytes(StandardCharsets.UTF_8);
+            } else {
+                data = entityImportExport.exportEntitiesToZIP(new ArrayList<>(selected), buildExportFetchPlan());
+            }
+            downloader.download(data, String.format("Categories.%s", downloadFormat.getFileExt()), downloadFormat);
+        } catch (Exception e) {
+            log.warn("Unable to export categories", e);
+            notifications.create(Notifications.NotificationType.ERROR)
+                    .withCaption(messages.getMessage(CategoryBrowse.class, "exportFailed"))
+                    .withDescription(e.getMessage())
+                    .show();
+        }
+    }
+
+    protected FetchPlan buildExportFetchPlan() {
+        return fetchPlans.builder(Category.class)
+                .addFetchPlan(FetchPlan.BASE)
+                .add("categoryAttrs", FetchPlan.BASE)
+                .build();
+    }
+
+    @Subscribe("importField")
+    public void onImportFieldFileUploadSucceed(SingleFileUploadField.FileUploadSucceedEvent event) {
+        try {
+            byte[] bytes = importField.getValue();
+            Collection<Object> importedEntities;
+            if (JSON.getFileExt().equals(Files.getFileExtension(event.getFileName()))) {
+                importedEntities = entityImportExport.importEntitiesFromJson(new String(bytes, StandardCharsets.UTF_8), createEntityImportPlan());
+            } else {
+                importedEntities = entityImportExport.importEntitiesFromZIP(bytes, createEntityImportPlan());
+            }
+
+            if (importedEntities.size() > 0) {
+                categoriesDl.load();
+                notifications.create(Notifications.NotificationType.HUMANIZED)
+                        .withDescription(messages.getMessage(CategoryBrowse.class, "importSuccessful"))
+                        .show();
+            }
+        } catch (Exception e) {
+            log.warn("Unable to import categories", e);
+            notifications.create(Notifications.NotificationType.ERROR)
+                    .withCaption(messages.getMessage(CategoryBrowse.class, "importFailed"))
+                    .withDescription(e.getMessage())
+                    .show();
+        }
+    }
+
+    protected EntityImportPlan createEntityImportPlan() {
+        return entityImportPlans.builder(Category.class)
+                .addLocalProperties()
+                .addProperty(new EntityImportPlanProperty(
+                        "categoryAttrs",
+                        entityImportPlans.builder(CategoryAttribute.class)
+                                .addLocalProperties()
+                                .addEmbeddedProperty(
+                                        "defaultEntity",
+                                        entityImportPlans.builder(ReferenceToEntity.class)
+                                                .addLocalProperties().
+                                                build()
+                                )
+                                .build(),
+                        CollectionImportPolicy.KEEP_ABSENT_ITEMS)
+                )
+                .build();
     }
 }

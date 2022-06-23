035b13c666300924955f2f2812428351b4b35860
Roman Pinyazhin <pinyazhin@haulmont.com>
2022-06-23 13:18:22 +0400

    [Flow UI] Error handling while opening view
    (cherry picked from commit fb720f7337cd3c93ad4bc4d12b42bc615d0ac28b)

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/sys/JmixServiceInitListener.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/sys/JmixServiceInitListener.java
@@ -1,12 +1,13 @@
 package io.jmix.flowui.sys;
 
 import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.InternalServerError;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteConfiguration;
-import com.vaadin.flow.server.ServiceInitEvent;
-import com.vaadin.flow.server.SessionInitEvent;
-import com.vaadin.flow.server.UIInitEvent;
-import com.vaadin.flow.server.VaadinServiceInitListener;
+import com.vaadin.flow.router.internal.ErrorTargetEntry;
+import com.vaadin.flow.server.*;
+import com.vaadin.flow.server.startup.ApplicationRouteRegistry;
+import io.jmix.flowui.component.error.JmixInternalServerError;
 import io.jmix.flowui.exception.UiExceptionHandlers;
 import io.jmix.flowui.screen.Screen;
 import io.jmix.flowui.screen.ScreenInfo;
@@ -17,6 +18,9 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Optional;
 
 @Component("flowui_JmixServiceInitListener")
 public class JmixServiceInitListener implements VaadinServiceInitListener, ApplicationContextAware {
@@ -48,6 +52,8 @@ public class JmixServiceInitListener implements VaadinServiceInitListener, Appli
         // the same name, see VaadinScanPackagesRegistrar#registerBeanDefinitions().
         // Register routes after route application scope is available.
         registerScreenRoutes();
+
+        registerInternalServiceError();
     }
 
     protected void onUIInitEvent(UIInitEvent uiInitEvent) {
@@ -79,5 +85,26 @@ public class JmixServiceInitListener implements VaadinServiceInitListener, Appli
 
             routeConfiguration.setRoute(route.value(), controllerClass);
         }
+    }
+
+    protected void registerInternalServiceError() {
+        ApplicationRouteRegistry applicationRouteRegistry =
+                ApplicationRouteRegistry.getInstance(VaadinService.getCurrent().getContext());
+
+        Optional<ErrorTargetEntry> navigationTargetOpt = applicationRouteRegistry.
+                getErrorNavigationTarget(new Exception());
+
+        if (navigationTargetOpt.isPresent()) {
+            ErrorTargetEntry errorTargetEntry = navigationTargetOpt.get();
+            if (!errorTargetEntry.getNavigationTarget().equals(InternalServerError.class)) {
+                log.debug("Internal server error handler is registered: "
+                        + errorTargetEntry.getNavigationTarget().getName());
+                return;
+            }
+        }
+
+        applicationRouteRegistry.setErrorNavigationTargets(Collections.singleton(JmixInternalServerError.class));
+
+        log.debug("Default internal server error handler is registered: " + JmixInternalServerError.class.getName());
     }
 }

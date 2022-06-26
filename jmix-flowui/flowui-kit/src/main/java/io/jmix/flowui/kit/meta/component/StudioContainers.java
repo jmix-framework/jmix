e56dc5ea54cf1fcecd191eddada369bf6df714bd
Gleb Durygin <g.durygin@haulmont.com>
2022-06-26 13:51:02 +0400

    Support Flow UI Screen Designer in framework jmix-framework/jmix#773
    Describe hbox and vbox components using meta-annotations

--- jmix-flowui/flowui-kit/src/main/java/io/jmix/flowui/kit/meta/component/StudioContainers.java
+++ jmix-flowui/flowui-kit/src/main/java/io/jmix/flowui/kit/meta/component/StudioContainers.java
@@ -1,6 +1,8 @@
 package io.jmix.flowui.kit.meta.component;
 
 import com.vaadin.flow.component.details.Details;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import io.jmix.flowui.kit.meta.StudioComponent;
 import io.jmix.flowui.kit.meta.StudioUiKit;
 import io.jmix.flowui.kit.meta.StudioProperty;
@@ -33,4 +35,86 @@ public interface StudioContainers {
             }
     )
     Details details();
+
+    @StudioComponent(
+            name = "HBox",
+            classFqn = "com.vaadin.flow.component.orderedlayout.HorizontalLayout",
+            category = "Containers",
+            xmlElement = "hbox",
+            icon = "io/jmix/flowui/kit/meta/icon/container/hbox.svg",
+            properties = {
+                    @StudioProperty(xmlAttribute = "alignItems", type = StudioPropertyType.ENUMERATION,
+                            classFqn = "com.vaadin.flow.component.orderedlayout.FlexComponent$Alignment",
+                            defaultValue = "STRETCH",
+                            options = {"START", "END", "CENTER", "STRETCH", "BASELINE", "AUTO"}),
+                    @StudioProperty(xmlAttribute = "boxSizing", type = StudioPropertyType.ENUMERATION,
+                            classFqn = "com.vaadin.flow.component.orderedlayout.BoxSizing", defaultValue = "UNDEFINED",
+                            options = {"UNDEFINED", "CONTENT_BOX", "BORDER_BOX"}),
+                    @StudioProperty(xmlAttribute = "className", type = StudioPropertyType.VALUES_LIST),
+                    @StudioProperty(xmlAttribute = "enabled", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "expand", type = StudioPropertyType.STRING),
+                    @StudioProperty(xmlAttribute = "height", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "id", type = StudioPropertyType.COMPONENT_ID),
+                    @StudioProperty(xmlAttribute = "justifyContent", type = StudioPropertyType.ENUMERATION,
+                            classFqn = "com.vaadin.flow.component.orderedlayout.FlexComponent$JustifyContentMode",
+                            setMethod = "setJustifyContentMode", defaultValue = "START",
+                            options = {"START", "END", "CENTER", "BETWEEN", "AROUND", "EVENLY"}),
+                    @StudioProperty(xmlAttribute = "margin", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "false"),
+                    @StudioProperty(xmlAttribute = "maxHeight", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "maxWidth", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "minHeight", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "minWidth", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "padding", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "false"),
+                    @StudioProperty(xmlAttribute = "spacing", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "visible", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "width", type = StudioPropertyType.SIZE)
+            }
+    )
+    HorizontalLayout hbox();
+
+    @StudioComponent(
+            name = "VBox",
+            classFqn = "com.vaadin.flow.component.orderedlayout.VerticalLayout",
+            category = "Containers",
+            xmlElement = "vbox",
+            icon = "io/jmix/flowui/kit/meta/icon/container/vbox.svg",
+            properties = {
+                    @StudioProperty(xmlAttribute = "alignItems", type = StudioPropertyType.ENUMERATION,
+                            classFqn = "com.vaadin.flow.component.orderedlayout.FlexComponent$Alignment",
+                            defaultValue = "STRETCH",
+                            options = {"START", "END", "CENTER", "STRETCH", "BASELINE", "AUTO"}),
+                    @StudioProperty(xmlAttribute = "boxSizing", type = StudioPropertyType.ENUMERATION,
+                            classFqn = "com.vaadin.flow.component.orderedlayout.BoxSizing", defaultValue = "UNDEFINED",
+                            options = {"UNDEFINED", "CONTENT_BOX", "BORDER_BOX"}),
+                    @StudioProperty(xmlAttribute = "className", type = StudioPropertyType.VALUES_LIST),
+                    @StudioProperty(xmlAttribute = "enabled", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "expand", type = StudioPropertyType.STRING),
+                    @StudioProperty(xmlAttribute = "height", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "id", type = StudioPropertyType.COMPONENT_ID),
+                    @StudioProperty(xmlAttribute = "justifyContent", type = StudioPropertyType.ENUMERATION,
+                            classFqn = "com.vaadin.flow.component.orderedlayout.FlexComponent$JustifyContentMode",
+                            setMethod = "setJustifyContentMode", defaultValue = "START",
+                            options = {"START", "END", "CENTER", "BETWEEN", "AROUND", "EVENLY"}),
+                    @StudioProperty(xmlAttribute = "margin", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "false"),
+                    @StudioProperty(xmlAttribute = "maxHeight", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "maxWidth", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "minHeight", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "minWidth", type = StudioPropertyType.SIZE),
+                    @StudioProperty(xmlAttribute = "padding", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "spacing", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "visible", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "width", type = StudioPropertyType.SIZE, defaultValue = "100%")
+            }
+    )
+    VerticalLayout vbox();
 }

4bb42e25ade2e463416250ca502cc8b26064e896
Timur Musin <musintd@haulmont.com>
2022-06-30 13:16:33 +0400

    Create Single Module Add-On (Kotlin) project template for Studio #799

---
+++ jmix-templates/content/project/single-module-addon-kotlin/${module_name}/src/test/kotlin/${project_rootPath}/${project_id.capitalize()}Test.kt
@@ -0,0 +1,13 @@
+package ${project_rootPackage}
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ${project_id.capitalize()}Test {
+
+    @Test
+    fun contextLoads() {
+
+    }
+}

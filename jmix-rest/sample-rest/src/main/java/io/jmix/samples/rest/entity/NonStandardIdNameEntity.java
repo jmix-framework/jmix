2b8f0ec3e20588088dba03ad6ab4bf3bcd70d8f2
Dmitry Taimanov <taimanov@haulmont.com>
2022-07-07 18:50:25 +0400

    REST API: Entity operation fail if identity attribute is not called "id" jmix-framework/jmix#775

---
+++ jmix-rest/sample-rest/src/main/java/io/jmix/samples/rest/entity/NonStandardIdNameEntity.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2022 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.samples.rest.entity;
+
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@JmixEntity
+@Table(name = "REST_NSIN_ENTITY")
+@Entity(name = "rest_NonStandardIdNameEntity")
+public class NonStandardIdNameEntity {
+    @Column(name = "CODE", nullable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer code;
+
+    @NotNull
+    @InstanceName
+    @Column(name = "NAME")
+    private String name;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

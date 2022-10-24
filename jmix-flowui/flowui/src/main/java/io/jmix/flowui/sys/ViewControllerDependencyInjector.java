e56c16f3a676e97ecb0436e36f856fcc0ea88973
Dmitry Kremnev <kremnev.dima@mail.ru>
2022-10-24 11:28:30 +0400

    DropdownButtonItems doesn't support ClickEvent subscription  jmix-framework/jmix#1116
    (cherry picked from commit 8debd8b783c9937bc8034329763440fcd7b302f6)

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/sys/ViewControllerDependencyInjector.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/sys/ViewControllerDependencyInjector.java
@@ -26,6 +26,7 @@ import io.jmix.flowui.component.UiComponentUtils;
 import io.jmix.flowui.facet.Facet;
 import io.jmix.flowui.kit.action.Action;
 import io.jmix.flowui.kit.component.HasActions;
+import io.jmix.flowui.kit.component.HasSubParts;
 import io.jmix.flowui.model.DataContext;
 import io.jmix.flowui.model.DataLoader;
 import io.jmix.flowui.model.InstallSubject;
@@ -631,6 +632,13 @@ public class ViewControllerDependencyInjector {
 
             if (componentOpt.isPresent()) {
                 Component component = componentOpt.get();
+
+                if (component instanceof HasSubParts) {
+                    Object part = ((HasSubParts) component).getSubPart(id);
+                    if (part != null) {
+                        return part;
+                    }
+                }
 
                 if (component instanceof HasActions) {
                     Action action = ((HasActions) component).getAction(id);

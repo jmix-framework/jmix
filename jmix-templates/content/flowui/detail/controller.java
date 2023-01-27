c618ef80c46464d9973bb3c92f2eb10c53415877
Mikhail Fedoseev <fedoseevmi@haulmont.com>
2023-01-27 14:56:17 +0400

    Binding to a non-existent class MainView.class in views controllers for addon #1394

--- jmix-templates/content/flowui/detail/controller.java
+++ jmix-templates/content/flowui/detail/controller.java
@@ -1,13 +1,17 @@
 package ${packageName};
 
 import ${entity.fqn};
+<%if (!api.jmixProjectModule.isApplication()) {%>
+import io.jmix.flowui.view.DefaultMainViewParent;
+<%} else {%>
 import ${module_basePackage}.view.main.MainView;
+<%}%>
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.view.*;
 
 <%if (classComment) {%>
 ${classComment}
-<%}%>@Route(value = "${detailRoute}/:${detailRouteParam}", layout = MainView.class)
+<%}%>@Route(value = "${detailRoute}/:${detailRouteParam}", layout = <%if (!api.jmixProjectModule.isApplication()) {%> DefaultMainViewParent.class <%} else {%>MainView.class<%}%>)
 @ViewController("${detailId}")
 @ViewDescriptor("${detailDescriptorName}.xml")
 @EditedEntityContainer("${dcId}")

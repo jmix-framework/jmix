df6df0a7e6dcccd119c5a9510b19215995351705
Gleb Gorelov <g.gorelov@haulmont.com>
2023-02-13 17:50:02 +0400

    FlowUI for Quartz add-on jmix-framework/jmix#1241

---
+++ jmix-quartz/quartz-flowui/src/main/java/io/jmix/quartzflowui/QuartzFlowUiConfiguration.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2020 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.quartzflowui;
+
+import io.jmix.core.annotation.JmixModule;
+import io.jmix.core.impl.scanning.AnnotationScanMetadataReaderFactory;
+import io.jmix.flowui.FlowuiConfiguration;
+import io.jmix.flowui.sys.ViewControllersConfiguration;
+import io.jmix.quartz.QuartzConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Collections;
+
+@Configuration
+@ComponentScan
+@ConfigurationPropertiesScan
+@JmixModule(dependsOn = {QuartzConfiguration.class, FlowuiConfiguration.class})
+@PropertySource(name = "io.jmix.quartzflowui", value = "classpath:/io/jmix/quartzflowui/module.properties")
+public class QuartzFlowUiConfiguration {
+
+    @Bean("flowui_quartz_QuartzViewControllers")
+    public ViewControllersConfiguration screens(ApplicationContext applicationContext,
+                                                AnnotationScanMetadataReaderFactory metadataReaderFactory) {
+        ViewControllersConfiguration viewControllers
+                = new ViewControllersConfiguration(applicationContext, metadataReaderFactory);
+        viewControllers.setBasePackages(Collections.singletonList("io.jmix.quartzflowui"));
+        return viewControllers;
+    }
+}
---
+++ jmix-quartz/quartz-flowui/src/main/java/io/jmix/quartzflowui/view/jobs/JobModelListView.java
@@ -0,0 +1,229 @@
+/*
+ * Copyright 2022 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.quartzflowui.view.jobs;
+
+import com.google.common.base.Strings;
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.router.Route;
+import io.jmix.core.MessageTools;
+import io.jmix.flowui.Notifications;
+import io.jmix.flowui.component.grid.DataGrid;
+import io.jmix.flowui.component.select.JmixSelect;
+import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.kit.action.ActionPerformedEvent;
+import io.jmix.flowui.model.CollectionContainer;
+import io.jmix.flowui.util.RemoveOperation;
+import io.jmix.flowui.view.*;
+import io.jmix.quartz.model.JobModel;
+import io.jmix.quartz.model.JobSource;
+import io.jmix.quartz.model.JobState;
+import io.jmix.quartz.service.QuartzService;
+import org.apache.commons.collections4.CollectionUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Comparator.*;
+import static org.apache.commons.lang3.StringUtils.containsIgnoreCase;
+
+@Route(value = "quartz/jobmodels", layout = DefaultMainViewParent.class)
+@ViewController("quartz_JobModel.list")
+@ViewDescriptor("job-model-list-view.xml")
+@LookupComponent("jobModelsTable")
+@DialogMode(width = "60em", height = "37.5em", resizable = true)
+public class JobModelListView extends StandardListView<JobModel> {
+
+    @ViewComponent
+    protected DataGrid<JobModel> jobModelsTable;
+    @ViewComponent
+    protected TypedTextField<String> nameFilter;
+    @ViewComponent
+    protected TypedTextField<String> classFilter;
+    @ViewComponent
+    protected TypedTextField<String> groupFilter;
+    @ViewComponent
+    protected JmixSelect<JobState> jobStateFilter;
+
+    @ViewComponent
+    protected CollectionContainer<JobModel> jobModelsDc;
+
+    @Autowired
+    protected RemoveOperation removeOperation;
+    @Autowired
+    protected QuartzService quartzService;
+    @Autowired
+    protected Notifications notifications;
+    @Autowired
+    protected MessageBundle messageBundle;
+    @Autowired
+    protected MessageTools messageTools;
+
+    @Subscribe
+    protected void onInit(View.InitEvent event) {
+        initTable();
+    }
+
+    protected void initTable() {
+        jobModelsTable.addColumn(entity -> entity.getLastFireDate() != null ?
+                        new SimpleDateFormat(messageBundle.getMessage("dateTimeWithSeconds"))
+                                .format(entity.getLastFireDate()) : "").setResizable(true)
+                .setHeader(messageTools.getPropertyCaption(jobModelsDc.getEntityMetaClass(), "lastFireDate"));
+
+        jobModelsTable.addColumn(entity -> entity.getNextFireDate() != null ?
+                        new SimpleDateFormat(messageBundle.getMessage("dateTimeWithSeconds"))
+                                .format(entity.getNextFireDate()) : "").setResizable(true)
+                .setHeader(messageTools.getPropertyCaption(jobModelsDc.getEntityMetaClass(), "nextFireDate"));
+    }
+
+    @Subscribe
+    protected void onBeforeShow(View.BeforeShowEvent event) {
+        nameFilter.addTypedValueChangeListener(this::onFilterFieldValueChange);
+        classFilter.addTypedValueChangeListener(this::onFilterFieldValueChange);
+        groupFilter.addTypedValueChangeListener(this::onFilterFieldValueChange);
+        jobStateFilter.addValueChangeListener(this::onFilterFieldValueChange);
+
+        loadJobsData();
+    }
+
+
+    protected void loadJobsData() {
+        List<JobModel> sortedJobs = quartzService.getAllJobs().stream()
+                .filter(jobModel -> (Strings.isNullOrEmpty(nameFilter.getTypedValue())
+                        || containsIgnoreCase(jobModel.getJobName(), nameFilter.getTypedValue()))
+                        && (Strings.isNullOrEmpty(classFilter.getTypedValue())
+                        || containsIgnoreCase(jobModel.getJobClass(), classFilter.getTypedValue()))
+                        && (Strings.isNullOrEmpty(groupFilter.getTypedValue())
+                        || containsIgnoreCase(jobModel.getJobGroup(), groupFilter.getTypedValue()))
+                        && (jobStateFilter.getValue() == null
+                        || jobStateFilter.getValue().equals(jobModel.getJobState())))
+                .sorted(comparing(JobModel::getJobState, nullsLast(naturalOrder()))
+                        .thenComparing(JobModel::getJobName))
+                .collect(Collectors.toList());
+
+        jobModelsDc.setItems(sortedJobs);
+    }
+
+    @Install(to = "jobModelsTable.executeNow", subject = "enabledRule")
+    protected boolean jobModelsTableExecuteNowEnabledRule() {
+        return !CollectionUtils.isEmpty(jobModelsTable.getSelectedItems())
+                && !isJobActive(jobModelsTable.getSingleSelectedItem());
+    }
+
+    @Install(to = "jobModelsTable.activate", subject = "enabledRule")
+    protected boolean jobModelsTableActivateEnabledRule() {
+        JobModel selectedJobModel = jobModelsTable.getSingleSelectedItem();
+        return selectedJobModel != null
+                && !isJobActive(selectedJobModel)
+                && CollectionUtils.isNotEmpty(selectedJobModel.getTriggers());
+    }
+
+    @Install(to = "jobModelsTable.deactivate", subject = "enabledRule")
+    protected boolean jobModelsTableDeactivateEnabledRule() {
+        return (jobModelsTable.getSingleSelectedItem() != null)
+                && isJobActive(jobModelsTable.getSingleSelectedItem());
+    }
+
+    @Install(to = "jobModelsTable.remove", subject = "enabledRule")
+    protected boolean jobModelsTableRemoveEnabledRule() {
+        JobModel selectedJobModel = jobModelsTable.getSingleSelectedItem();
+        return selectedJobModel != null
+                && !isJobActive(selectedJobModel)
+                && JobSource.USER_DEFINED.equals(selectedJobModel.getJobSource());
+    }
+
+    @Subscribe("jobModelsTable.executeNow")
+    protected void onJobModelsTableExecuteNow(ActionPerformedEvent event) {
+        JobModel selectedJobModel = jobModelsTable.getSingleSelectedItem();
+        if (selectedJobModel == null) {
+            return;
+        }
+
+        quartzService.executeNow(selectedJobModel.getJobName(), selectedJobModel.getJobGroup());
+        notifications.create(messageBundle.formatMessage("jobExecuted", selectedJobModel.getJobName()))
+                .withType(Notifications.Type.DEFAULT)
+                .show();
+        loadJobsData();
+    }
+
+    @Subscribe("jobModelsTable.activate")
+    protected void onJobModelsTableActivate(ActionPerformedEvent event) {
+        JobModel selectedJobModel = jobModelsTable.getSingleSelectedItem();
+        if (selectedJobModel == null) {
+            return;
+        }
+
+        quartzService.resumeJob(selectedJobModel.getJobName(), selectedJobModel.getJobGroup());
+        notifications.create(messageBundle.formatMessage("jobResumed", selectedJobModel.getJobName()))
+                .withType(Notifications.Type.DEFAULT)
+                .show();
+
+        loadJobsData();
+    }
+
+    @Subscribe("jobModelsTable.deactivate")
+    protected void onJobModelsTableDeactivate(ActionPerformedEvent event) {
+        JobModel selectedJobModel = jobModelsTable.getSingleSelectedItem();
+        quartzService.pauseJob(selectedJobModel.getJobName(), selectedJobModel.getJobGroup());
+        notifications.create(messageBundle.formatMessage("jobPaused", selectedJobModel.getJobName()))
+                .withType(Notifications.Type.DEFAULT)
+                .show();
+
+        loadJobsData();
+    }
+
+    @Subscribe("jobModelsTable.remove")
+    protected void onJobModelsTableRemove(ActionPerformedEvent event) {
+        removeOperation.builder(jobModelsTable)
+                .withConfirmation(true)
+                .beforeActionPerformed(e -> {
+                    if (CollectionUtils.isNotEmpty(e.getItems())) {
+                        JobModel jobToDelete = e.getItems().get(0);
+                        quartzService.deleteJob(jobToDelete.getJobName(), jobToDelete.getJobGroup());
+                        notifications.create(messageBundle.formatMessage("jobDeleted", jobToDelete.getJobName()))
+                                .withType(Notifications.Type.DEFAULT)
+                                .show();
+                        loadJobsData();
+                    }
+                })
+                .remove();
+    }
+
+    @Subscribe("jobModelsTable.refresh")
+    protected void onJobModelsTableRefresh(ActionPerformedEvent event) {
+        loadJobsData();
+    }
+
+    @Install(to = "jobModelsTable.create", subject = "afterSaveHandler")
+    protected void jobModelsTableCreateAfterCommitHandler(JobModel jobModel) {
+        loadJobsData();
+    }
+
+    @Install(to = "jobModelsTable.edit", subject = "afterSaveHandler")
+    protected void jobModelsTableEditAfterCommitHandler(JobModel jobModel) {
+        loadJobsData();
+    }
+
+    protected boolean isJobActive(JobModel jobModel) {
+        return jobModel != null && jobModel.getJobState() == JobState.NORMAL;
+    }
+
+    protected void onFilterFieldValueChange(ComponentEvent<?> event) {
+        loadJobsData();
+    }
+}

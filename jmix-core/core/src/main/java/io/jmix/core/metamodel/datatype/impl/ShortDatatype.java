f422fe01bdd2d420cd907e65d785b54ae517526b
Maxim Gorbunkov <m.gorbunkov@haulmont.com>
2023-02-28 16:52:57 +0400

    Remove Ddl annotation from Short, Float and BigInteger datatypes
    Studio itself will decide which database type to use
    
    (cherry picked from commit df1649800ef66927ca0da7f69de14f8b41a77830)

--- jmix-core/core/src/main/java/io/jmix/core/metamodel/datatype/impl/ShortDatatype.java
+++ jmix-core/core/src/main/java/io/jmix/core/metamodel/datatype/impl/ShortDatatype.java
@@ -17,7 +17,6 @@
 package io.jmix.core.metamodel.datatype.impl;
 
 import io.jmix.core.metamodel.annotation.DatatypeDef;
-import io.jmix.core.metamodel.annotation.Ddl;
 import io.jmix.core.metamodel.datatype.Datatype;
 import io.jmix.core.metamodel.datatype.FormatStrings;
 import io.jmix.core.metamodel.datatype.FormatStringsRegistry;
@@ -35,7 +34,6 @@ import java.util.Locale;
 @io.jmix.core.metamodel.annotation.NumberFormat(
         pattern = "0"
 )
-@Ddl("SMALLINT")
 public class ShortDatatype extends NumberDatatype implements Datatype<Short> {
 
     @Autowired

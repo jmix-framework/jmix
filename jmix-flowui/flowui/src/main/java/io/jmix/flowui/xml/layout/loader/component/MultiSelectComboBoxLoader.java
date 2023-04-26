102546781b650a57b7709416d1c702dc49827532
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-04-26 14:24:10 +0400

    MultiSelectComboBox can't be defined without binding in XML jmix-framework/jmix#1618

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/xml/layout/loader/component/MultiSelectComboBoxLoader.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/xml/layout/loader/component/MultiSelectComboBoxLoader.java
@@ -18,7 +18,6 @@ package io.jmix.flowui.xml.layout.loader.component;
 
 import io.jmix.core.Metadata;
 import io.jmix.flowui.component.multiselectcombobox.JmixMultiSelectComboBox;
-import io.jmix.flowui.exception.GuiDevelopmentException;
 import io.jmix.flowui.xml.layout.loader.AbstractComponentLoader;
 import io.jmix.flowui.xml.layout.support.DataLoaderSupport;
 
@@ -67,16 +66,6 @@ public class MultiSelectComboBoxLoader extends AbstractComponentLoader<JmixMulti
 
         if (resultComponent.getValueSource() == null) {
             loadMetaClass();
-
-            if (resultComponent.getMetaClass() == null) {
-                String message = String.format(
-                        "%s doesn't have data binding. Set either dataContainer and property or metaClass attribute.",
-                        resultComponent.getClass().getSimpleName()
-                );
-
-                throw new GuiDevelopmentException(message,
-                        context, "Component ID", resultComponent.getId().orElse("null"));
-            }
         }
     }
 

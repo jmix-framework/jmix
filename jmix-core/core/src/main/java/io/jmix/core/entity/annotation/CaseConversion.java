d63a254c1a431ad95362ab6a061a70e6589f51ab
Ilya Kuchmin <kuchmin@haulmont.com>
2023-05-22 16:03:59 +0400

    Allow using CaseConversion in composed annotations (same as BeanValidation annotations)

--- jmix-core/core/src/main/java/io/jmix/core/entity/annotation/CaseConversion.java
+++ jmix-core/core/src/main/java/io/jmix/core/entity/annotation/CaseConversion.java
@@ -21,11 +21,18 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+
 /**
  * Indicates that auto case conversion should be used for text input fields bound with marked entity field.
  */
 
-@Target({ElementType.FIELD})
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
 @MetaAnnotation
 public @interface CaseConversion {

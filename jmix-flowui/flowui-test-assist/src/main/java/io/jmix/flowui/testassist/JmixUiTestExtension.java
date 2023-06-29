1d75f68aa31fc63eba9a29beb2ee378299e98419
Gleb Gorelov <g.gorelov@haulmont.com>
2023-06-29 13:20:56 +0400

    Update to the latest Vaadin version #1922 (fix tests)

--- jmix-flowui/flowui-test-assist/src/main/java/io/jmix/flowui/testassist/JmixUiTestExtension.java
+++ jmix-flowui/flowui-test-assist/src/main/java/io/jmix/flowui/testassist/JmixUiTestExtension.java
@@ -201,6 +201,8 @@ public class JmixUiTestExtension implements TestInstancePostProcessor, BeforeEac
         vaadinSession = new TestVaadinSession(springServlet.getService());
         VaadinSession.setCurrent(vaadinSession);
 
+        vaadinSession.setConfiguration(springServlet.getService().getDeploymentConfiguration());
+
         TestVaadinRequest request = new TestVaadinRequest(springServlet.getService());
         CurrentInstance.set(VaadinRequest.class, request);
 

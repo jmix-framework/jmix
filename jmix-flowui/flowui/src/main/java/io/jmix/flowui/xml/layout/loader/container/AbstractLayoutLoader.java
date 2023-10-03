35b7d20b872108bf9a4d5dfcf2e6d13005753128
Dmitry Kremnev <kremnev.dima@mail.ru>
2023-10-03 12:24:40 +0400

    Ability to define theme names for layouts jmix-framework/jmix_2293

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/xml/layout/loader/container/AbstractLayoutLoader.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/xml/layout/loader/container/AbstractLayoutLoader.java
@@ -16,9 +16,14 @@
 
 package io.jmix.flowui.xml.layout.loader.container;
 
+import com.google.common.base.Strings;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.ThemableLayout;
+import org.dom4j.Element;
+
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class AbstractLayoutLoader<T extends Component & ThemableLayout & FlexComponent>
         extends AbstractContainerLoader<T> {
@@ -34,7 +39,28 @@ public abstract class AbstractLayoutLoader<T extends Component & ThemableLayout 
         componentLoader().loadThemableAttributes(resultComponent, element);
         componentLoader().loadFlexibleAttributes(resultComponent, element);
         componentLoader().loadEnabled(resultComponent, element);
+        loadThemeNames(resultComponent, element);
 
         loadSubComponentsAndExpand(resultComponent, element);
+    }
+
+    protected void loadThemeNames(ThemableLayout resultComponent, Element element) {
+        loaderSupport.loadString(element, "themeNames")
+                .ifPresent(themesString -> {
+                    List<String> themeNames = split(themesString);
+
+                    if (!themeNames.isEmpty()) {
+                        // To unset the previous default spacing value
+                        resultComponent.setSpacing(false);
+
+                        resultComponent.getThemeList().addAll(themeNames);
+                    }
+                });
+    }
+
+    protected List<String> split(String names) {
+        return Arrays.stream(names.split("[\\s,]+"))
+                .filter(split -> !Strings.isNullOrEmpty(split))
+                .toList();
     }
 }

2812d496e5cd18d2e2b5052380f3d27e73d51efe
Yuriy Khrebtov <i.khrebtov@haulmont.net>
2023-10-18 12:35:44 +0400

    EntityRestore bean assumes that deletedDate attribute has the Date type #2349

--- jmix-data/data/src/main/java/io/jmix/data/impl/converters/AuditConverters.java
+++ jmix-data/data/src/main/java/io/jmix/data/impl/converters/AuditConverters.java
@@ -19,7 +19,10 @@ package io.jmix.data.impl.converters;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.Date;
 
 import static java.time.ZoneId.systemDefault;
@@ -62,4 +65,102 @@ public class AuditConverters {
         }
     }
 
+    public enum OffsetDateTimeToDateConverter implements Converter<OffsetDateTime, Date> {
+        INSTANCE;
+
+        @Override
+        public Date convert(OffsetDateTime source) {
+            return Date.from(source.toInstant());
+        }
+    }
+
+    public enum DateToLocalDateConverter implements Converter<Date, LocalDate> {
+        INSTANCE;
+
+        @Override
+        public LocalDate convert(Date source) {
+            return LocalDate.ofInstant(source.toInstant(), ZoneId.systemDefault());
+        }
+    }
+
+    public enum LocalDateToDateConverter implements Converter<LocalDate, Date> {
+        INSTANCE;
+
+        @Override
+        public Date convert(LocalDate source) {
+            return Date.from(source.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        }
+    }
+
+    public enum DateToLocalDateTimeConverter implements Converter<Date, LocalDateTime> {
+        INSTANCE;
+
+        @Override
+        public LocalDateTime convert(Date source) {
+            return LocalDateTime.ofInstant(source.toInstant(), ZoneId.systemDefault());
+        }
+    }
+
+    public enum LocalDateTimeToDateConverter implements Converter<LocalDateTime, Date> {
+        INSTANCE;
+
+        @Override
+        public Date convert(LocalDateTime source) {
+            return Date.from(source.atZone(ZoneId.systemDefault()).toInstant());
+        }
+    }
+
+    public enum LocalDateTimeToLocalDateConverter implements Converter<LocalDateTime, LocalDate> {
+        INSTANCE;
+
+        @Override
+        public LocalDate convert(LocalDateTime source) {
+            return source.toLocalDate();
+        }
+    }
+
+    public enum LocalDateToLocalDateTimeConverter implements Converter<LocalDate, LocalDateTime> {
+        INSTANCE;
+
+        @Override
+        public LocalDateTime convert(LocalDate source) {
+            return LocalDateTime.from(source);
+        }
+    }
+
+    public enum LocalDateToOffsetDateTimeConverter implements Converter<LocalDate, OffsetDateTime> {
+        INSTANCE;
+
+        @Override
+        public OffsetDateTime convert(LocalDate source) {
+            return OffsetDateTime.from(source);
+        }
+    }
+
+    public enum OffsetDateTimeToLocalDateConverter implements Converter<OffsetDateTime, LocalDate> {
+        INSTANCE;
+
+        @Override
+        public LocalDate convert(OffsetDateTime source) {
+            return source.toLocalDate();
+        }
+    }
+
+    public enum LocalDateTimeToOffsetDateTimeConverter implements Converter<LocalDateTime, OffsetDateTime> {
+        INSTANCE;
+
+        @Override
+        public OffsetDateTime convert(LocalDateTime source) {
+            return OffsetDateTime.from(source);
+        }
+    }
+
+    public enum OffsetDateTimeToLocalDateTimeConverter implements Converter<OffsetDateTime, LocalDateTime> {
+        INSTANCE;
+
+        @Override
+        public LocalDateTime convert(OffsetDateTime source) {
+            return source.toLocalDateTime();
+        }
+    }
 }

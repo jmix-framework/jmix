309f4227abedcc49b1156498059430ae50167b5e
Yuriy Khrebtov <i.khrebtov@haulmont.net>
2023-12-26 11:11:47 +0400

    Add hint in comment 2137

--- jmix-templates/content/flowui/main-view-top-menu/controller.java
+++ jmix-templates/content/flowui/main-view-top-menu/controller.java
@@ -5,6 +5,12 @@ import io.jmix.flowui.app.main.StandardMainView;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
 
+/*
+ * To use the view as a main view, please, don't forget to set 'jmix.ui.main-view-id' property
+ * to new value (see @ViewController annotation).
+ * Also, please, check that the route of default MainView is not the same
+ * as the route of this view (see @Route annotation).
+ */
 @Route("")
 @ViewController("${id}")
 @ViewDescriptor("${descriptorName}.xml")

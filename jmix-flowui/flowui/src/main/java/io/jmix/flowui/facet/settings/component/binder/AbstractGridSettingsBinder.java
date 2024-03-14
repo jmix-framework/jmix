39b08babbe651bb1f03b16c8fb725e0a019a2e76
Ilia Grachev <i.grachev@haulmont.net>
2024-03-14 13:29:52 +0400

    Add flexGrow to settings 2965

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/facet/settings/component/binder/AbstractGridSettingsBinder.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/facet/settings/component/binder/AbstractGridSettingsBinder.java
@@ -59,7 +59,9 @@ public abstract class AbstractGridSettingsBinder<V extends Grid<?>, S extends Da
                 Objects.requireNonNull(column);
 
                 if (sColumn.getWidth() != null) {
-                    column.setFlexGrow(0);
+                    column.setFlexGrow(sColumn.getFlexGrow() > 0 && !"100px".equals(sColumn.getWidth())
+                            ? 0
+                            : sColumn.getFlexGrow());
                     column.setWidth(sColumn.getWidth());
                 }
                 if (sColumn.getVisible() != null) {
@@ -230,6 +232,7 @@ public abstract class AbstractGridSettingsBinder<V extends Grid<?>, S extends Da
                     sColumn.setKey(column.getKey());
                     sColumn.setWidth(column.getWidth());
                     sColumn.setVisible(column.isVisible());
+                    sColumn.setFlexGrow(column.getFlexGrow());
                     return sColumn;
                 }).toList();
 

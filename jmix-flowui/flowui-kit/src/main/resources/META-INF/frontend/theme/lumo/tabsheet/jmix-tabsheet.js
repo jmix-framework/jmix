ed6b372d42029e5dfddf67acc95954050bfe6975
Gleb Gorelov <glebfox@gmail.com>
2024-04-09 22:21:02 +0400

    Remove FlowUI modules from Jmix 1.6 #1676

--- jmix-flowui/flowui-kit/src/main/resources/META-INF/frontend/theme/lumo/tabsheet/jmix-tabsheet.js
+++
@@ -1,5 +0,0 @@
-// CAUTION: copied from @vaadin/tabsheet/theme/lumo/vaadin-tabsheet.js [since Vaadin 23.3.0]
-import '@vaadin/tabs/theme/lumo/vaadin-tabs.js';
-import '@vaadin/scroller/theme/lumo/vaadin-scroller.js';
-import './jmix-tabsheet-styles.js';
-import '../../../src/tabsheet/jmix-tabsheet.js';

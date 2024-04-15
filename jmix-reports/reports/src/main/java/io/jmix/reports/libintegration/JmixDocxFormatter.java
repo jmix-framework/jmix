fa97ed7cd412b93ecf25315de2b20a31af3a76a3
Aleksey Oblozhko <a.oblozhko@haulmont.net>
2024-04-15 13:46:24 +0300

    NPE when generating a DOCX report with a formatter (release 2.2) #3122 (3143)

---
+++ jmix-reports/reports/src/main/java/io/jmix/reports/libintegration/JmixDocxFormatter.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.reports.libintegration;
+
+import io.jmix.reports.yarg.formatters.factory.FormatterFactoryInput;
+import io.jmix.reports.yarg.formatters.impl.DocxFormatter;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component("report_JmixDocxFormatter")
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+public class JmixDocxFormatter extends DocxFormatter {
+
+    public JmixDocxFormatter(FormatterFactoryInput formatterFactoryInput) {
+        super(formatterFactoryInput);
+    }
+}

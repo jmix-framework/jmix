97fd9c0731e6e71e15a40aa1b4b0761d4801c0a1
Dmitry Taimanov <taimanov@haulmont.com>
2024-04-18 17:07:10 +0400

    [port from 1.6] SOFT_DELETION hints set incorrectly in SingleValueOwningPropertyHolder and SingleValueMappedByPropertyHolder jmix-framework/jmix#3170

--- jmix-data/eclipselink/src/main/java/io/jmix/eclipselink/impl/lazyloading/SingleValueMappedByPropertyHolder.java
+++ jmix-data/eclipselink/src/main/java/io/jmix/eclipselink/impl/lazyloading/SingleValueMappedByPropertyHolder.java
@@ -22,7 +22,6 @@ import io.jmix.core.entity.EntityValues;
 import io.jmix.core.impl.SerializationContext;
 import io.jmix.core.metamodel.model.MetaClass;
 import io.jmix.core.metamodel.model.MetaProperty;
-import io.jmix.data.PersistenceHints;
 import org.eclipse.persistence.indirection.ValueHolderInterface;
 import org.springframework.beans.factory.BeanFactory;
 
@@ -75,18 +74,12 @@ public class SingleValueMappedByPropertyHolder extends AbstractSingleValueHolder
                                 .add(getPropertyInfo().getName(), builder -> builder.addFetchPlan(FetchPlan.BASE))
                                 .build())
                 .setAccessConstraints(getLoadOptions().getAccessConstraints())
-                .setHints(getLoadOptions().getHints())
-                .setHint(PREV_SOFT_DELETION,
-                        getLoadOptions().getHints().getOrDefault(PersistenceHints.SOFT_DELETION, SOFT_DELETION_ABSENT))
-                .setHint(PersistenceHints.SOFT_DELETION, false);
+                .setHints(getLoadOptions().getHints());
     }
 
     protected LoadContext<?> createLoadContextByInverseProperty(MetaClass metaClass, String primaryKeyName) {
         LoadContext<?> loadContext = new LoadContext<>(metaClass)
-                .setHints(getLoadOptions().getHints())
-                .setHint(PREV_SOFT_DELETION,
-                        getLoadOptions().getHints().getOrDefault(PersistenceHints.SOFT_DELETION, SOFT_DELETION_ABSENT))
-                .setHint(PersistenceHints.SOFT_DELETION, false);
+                .setHints(getLoadOptions().getHints());
         loadContext.setQueryString(String.format("select e from %s e where e.%s.%s = :entityId", metaClass.getName(),
                 getPropertyInfo().getInversePropertyName(), primaryKeyName))
                 .setParameter("entityId", Objects.requireNonNull(EntityValues.getId(getOwner())));

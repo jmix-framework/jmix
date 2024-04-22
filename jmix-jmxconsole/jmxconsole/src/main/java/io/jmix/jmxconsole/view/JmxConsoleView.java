946b6a4180cbc69166762ee201a16275fc46cab5
Aleksey Oblozhko <a.oblozhko@haulmont.net>
2024-04-22 12:27:11 +0300

    Filter in JMX Console should be reflected in URL (release 2.2) #2489

--- jmix-jmxconsole/jmxconsole/src/main/java/io/jmix/jmxconsole/view/JmxConsoleView.java
+++ jmix-jmxconsole/jmxconsole/src/main/java/io/jmix/jmxconsole/view/JmxConsoleView.java
@@ -18,11 +18,16 @@ package io.jmix.jmxconsole.view;
 
 
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.router.QueryParameters;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteParameters;
 import io.jmix.core.LoadContext;
 import io.jmix.flowui.component.grid.DataGrid;
 import io.jmix.flowui.component.textfield.TypedTextField;
+import io.jmix.flowui.facet.UrlQueryParametersFacet;
+import io.jmix.flowui.facet.urlqueryparameters.AbstractUrlQueryParametersBinder;
 import io.jmix.flowui.model.CollectionLoader;
 import io.jmix.flowui.view.*;
 import io.jmix.flowui.view.navigation.UrlParamSerializer;
@@ -32,6 +37,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,6 +47,9 @@ import java.util.stream.Collectors;
 @ViewDescriptor("jmx-console-view.xml")
 @LookupComponent("mbeansDataGrid")
 public class JmxConsoleView extends StandardView {
+
+    private static final String SEARCH_URL_PARAM = "search";
+
     @ViewComponent
     protected DataGrid<ManagedBeanInfo> mbeansDataGrid;
     @ViewComponent
@@ -50,16 +59,24 @@ public class JmxConsoleView extends StandardView {
 
     @Autowired
     protected JmxControl jmxControl;
+
     @Autowired
     protected UrlParamSerializer urlParamSerializer;
+    @ViewComponent
+    private UrlQueryParametersFacet urlQueryParameters;
 
     @Subscribe
     public void onInit(final InitEvent event) {
         initSearchField();
+        initUrlParameters();
     }
 
     protected void initSearchField() {
         mbeanSearchField.addTypedValueChangeListener(valueChangeEvent -> mbeanDl.load());
+    }
+
+    protected void initUrlParameters() {
+        urlQueryParameters.registerBinder(new JmxConsoleQueryParametersBinder());
     }
 
     @Install(to = "mbeanDl", target = Target.DATA_LOADER)
@@ -87,5 +104,29 @@ public class JmxConsoleView extends StandardView {
                         StringUtils.containsIgnoreCase(managedBeanInfo.getObjectName(), objectName))
                 .sorted(Comparator.comparing(ManagedBeanInfo::getDomain))
                 .collect(Collectors.toList());
+    }
+
+    private class JmxConsoleQueryParametersBinder extends AbstractUrlQueryParametersBinder {
+        public JmxConsoleQueryParametersBinder() {
+            mbeanSearchField.addValueChangeListener(event -> {
+                String text = event.getValue();
+                QueryParameters qp = new QueryParameters(ImmutableMap.of(SEARCH_URL_PARAM,
+                        text != null ? Collections.singletonList(text) : Collections.emptyList()));
+                fireQueryParametersChanged(new UrlQueryParametersFacet.UrlQueryParametersChangeEvent(this, qp));
+            });
+        }
+
+        @Override
+        public void updateState(QueryParameters queryParameters) {
+            List<String> jobNameStrings = queryParameters.getParameters().get(SEARCH_URL_PARAM);
+            if (jobNameStrings != null && !jobNameStrings.isEmpty()) {
+                mbeanSearchField.setValue(jobNameStrings.get(0));
+            }
+        }
+
+        @Override
+        public Component getComponent() {
+            return null;
+        }
     }
 }

be19f82b7f42c4358866d96343dc40d2ce2f26d2
Aleksey Oblozhko <a.oblozhko@haulmont.net>
2024-04-23 14:33:37 +0400

    Use fetch callback to load users while searching in EntityLogView 3034

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/facet/settings/component/binder/AbstractGridSettingsBinder.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/facet/settings/component/binder/AbstractGridSettingsBinder.java
@@ -113,7 +113,7 @@ public abstract class AbstractGridSettingsBinder<V extends Grid<?>, S extends Da
     protected List<? extends Grid.Column<?>> getApplicableColumns(V component) {
         List<? extends Grid.Column<?>> componentColumns = getOrderedColumns(component);
         if (componentColumns.stream().anyMatch(c -> c.getKey() == null)) {
-            log.debug("Grid has column without key specified, settings for it would not be stored");
+            log.debug("{} has column without key specified, settings for it would not be stored", component.getClass().getSimpleName());
             componentColumns = componentColumns.stream()
                     .filter(c -> c.getKey() != null).toList();
         }

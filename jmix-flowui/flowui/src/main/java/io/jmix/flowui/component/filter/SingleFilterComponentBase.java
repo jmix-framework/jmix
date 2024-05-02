f4a8d55b61d4977916e90e2fb30983f84a133b4c
Ilia Grachev <ilya.grachev@gmail.com>
2024-05-02 12:23:18 +0400

    Generic Filter reloads data loaders redundandly #3092

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/component/filter/SingleFilterComponentBase.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/component/filter/SingleFilterComponentBase.java
@@ -201,7 +201,10 @@ public abstract class SingleFilterComponentBase<V> extends CustomField<V>
 
     @Override
     public void apply() {
-        if (dataLoader != null) {
+        //If a filter condition has a default value then DataLoader loads data when GenericFilter is initialised.
+        // So if we have several such conditions we get redundant data loading.
+        // To avoid this problem FilterComponent skips data loading if it's not attached to the UI.
+        if (isAttached() && dataLoader != null) {
             setupLoaderFirstResult();
             if (autoApply) {
                 dataLoader.load();

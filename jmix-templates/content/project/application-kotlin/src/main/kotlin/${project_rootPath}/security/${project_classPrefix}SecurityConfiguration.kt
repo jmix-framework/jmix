01ce532b670a5b1ff5439f1b1d8013788627d4e7
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-05-17 19:02:46 +0400

    Minor changes

--- jmix-templates/content/project/application-kotlin/src/main/kotlin/${project_rootPath}/security/${project_classPrefix}SecurityConfiguration.kt
+++ jmix-templates/content/project/application-kotlin/src/main/kotlin/${project_rootPath}/security/${project_classPrefix}SecurityConfiguration.kt
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.web.SecurityFilterChain
 
 /**
- * This configuration complements standard security configurations that come from Jmix add-ons (security-flowui, oidc,
+ * This configuration complements standard security configurations that come from Jmix modules (security-flowui, oidc,
  * authserver).
- *
- * If you need to define own [SecurityFilterChain] to configure custom API endpoints security, you can do it in
- * this class. In mose cases, custom SecurityFilterChain must be applied first, so the proper
+ * <p>
+ * You can configure custom API endpoints security by defining [SecurityFilterChain] beans in this class.
+ * In most cases, custom SecurityFilterChain must be applied first, so the proper
  * [org.springframework.core.annotation.Order] should be defined for the bean. The order value from the
  * [io.jmix.core.JmixSecurityFilterChainOrder#CUSTOM] is guaranteed to be smaller than any other filter chain
  * order from Jmix.
--- jmix-templates/content/project/application/src/main/java/${project_rootPath}/security/${project_classPrefix}SecurityConfiguration.java
+++ jmix-templates/content/project/application/src/main/java/${project_rootPath}/security/${project_classPrefix}SecurityConfiguration.java
@@ -4,11 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
- * This configuration complements standard security configurations that come from Jmix add-ons (security-flowui, oidc,
+ * This configuration complements standard security configurations that come from Jmix modules (security-flowui, oidc,
  * authserver).
  * <p>
- * If you need to define own {@link SecurityFilterChain} to configure custom API endpoints security, you can do it in
- * this class. In mose cases, custom SecurityFilterChain must be applied first, so the proper
+ * You can configure custom API endpoints security by defining {@link SecurityFilterChain} beans in this class.
+ * In most cases, custom SecurityFilterChain must be applied first, so the proper
  * {@link org.springframework.core.annotation.Order} should be defined for the bean. The order value from the
  * {@link io.jmix.core.JmixSecurityFilterChainOrder#CUSTOM} is guaranteed to be smaller than any other filter chain
  * order from Jmix.

a03d684e0a4a390ae09d2a274aa1580bdea7656d
aleksandrovpv <p.aleksandrov@haulmont.net>
2024-05-24 13:46:06 +0400

    SearchSettingsComparatorTest

--- jmix-search/search/src/test/java/io/jmix/search/index/impl/SearchSettingsComparatorTest.java
+++ jmix-search/search/src/test/java/io/jmix/search/index/impl/SearchSettingsComparatorTest.java
@@ -16,13 +16,52 @@
 
 package io.jmix.search.index.impl;
 
+import org.apache.poi.ss.formula.functions.LookupUtils;
+import org.elasticsearch.common.settings.Settings;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class SearchSettingsComparatorTest {
 
     @Test
-    void compare() {
+    void compare_equal() {
+        SearchSettingsComparator searchSettingsComparator = new SearchSettingsComparator();
+        Settings indexSettings = mockSettings(Map.of("setting1", "value1", "setting2", "value2"));
+        Settings applicationSetting = mockSettings(Map.of("setting1", "value1", "setting2", "value2"));
+        ComparingState result = searchSettingsComparator.compare(indexSettings, applicationSetting);
+        assertEquals(ComparingState.EQUAL, result);
+    }
+
+    @Test
+    void compare_equal_more_settings_in_index() {
+        SearchSettingsComparator searchSettingsComparator = new SearchSettingsComparator();
+        Settings indexSettings = mockSettings(Map.of("setting1", "value1", "setting2", "value2", "setting3", "value3"));
+        Settings applicationSetting = mockSettings(Map.of("setting1", "value1", "setting2", "value2"));
+        ComparingState result = searchSettingsComparator.compare(indexSettings, applicationSetting);
+        assertEquals(ComparingState.EQUAL, result);
+    }
+
+    @Test
+    void compare_additional_setting_in_application() {
+        SearchSettingsComparator searchSettingsComparator = new SearchSettingsComparator();
+        Settings indexSettings = mockSettings(Map.of("setting1", "value1", "setting2", "value2"));
+        Settings applicationSetting = mockSettings(Map.of("setting1", "value1", "setting2", "value2", "setting3", "value3"));
+        ComparingState result = searchSettingsComparator.compare(indexSettings, applicationSetting);
+        assertEquals(ComparingState.NOT_COMPATIBLE, result);
+    }
+
+    private static Settings mockSettings(Map<String, Object> settingsMap) {
+        Settings mock = mock(Settings.class);
+        when(mock.keySet()).thenReturn(settingsMap.keySet());
+        when(mock.get(anyString())).thenAnswer(invocation -> {
+           String key = invocation.getArgument(0);
+           return settingsMap.get(key);
+        });
+        return mock;
     }
 }

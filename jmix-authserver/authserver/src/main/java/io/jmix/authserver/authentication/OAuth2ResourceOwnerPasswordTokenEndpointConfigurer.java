3c39d4fc3f36fba00fcdd3b0100d4fb04b1a0865
Maxim Gorbunkov <m.gorbunkov@haulmont.com>
2024-05-31 17:00:54 +0400

    Fixed an issue with calling the Auth2AuthorizationServerConfigurer#init() twice #2499

--- jmix-authserver/authserver/src/main/java/io/jmix/authserver/authentication/OAuth2ResourceOwnerPasswordTokenEndpointConfigurer.java
+++ jmix-authserver/authserver/src/main/java/io/jmix/authserver/authentication/OAuth2ResourceOwnerPasswordTokenEndpointConfigurer.java
@@ -7,38 +7,37 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.token.OAuth2TokenGenerator;
-import org.springframework.stereotype.Component;
 
 /**
- * The class configures the token endpoint
- * by registering {@link OAuth2ResourceOwnerPasswordCredentialsAuthenticationConverter} and
+ * The class configures the token endpoint by registering
+ * {@link OAuth2ResourceOwnerPasswordCredentialsAuthenticationConverter} and
  * {@link OAuth2ResourceOwnerPasswordCredentialsAuthenticationProvider} which are required for OAuth 2.0 Resource Owner
  * Password Credentials grant.
  *
  * @see OAuth2ResourceOwnerPasswordCredentialsAuthenticationConverter
  * @see OAuth2ResourceOwnerPasswordCredentialsAuthenticationProvider
  */
-@Component
 public class OAuth2ResourceOwnerPasswordTokenEndpointConfigurer extends AbstractHttpConfigurer<OAuth2ResourceOwnerPasswordTokenEndpointConfigurer, HttpSecurity> {
 
     @Override
     public void init(HttpSecurity http) throws Exception {
         OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = http.getConfigurer(OAuth2AuthorizationServerConfigurer.class);
-        http.with(authorizationServerConfigurer, configurer -> {
-            OAuth2TokenGenerator<?> tokenGenerator = http.getSharedObject(OAuth2TokenGenerator.class);
-            ApplicationContext applicationContext = http.getSharedObject(ApplicationContext.class);
-            AuthenticationManager authenticationManager = applicationContext.getBean(AuthenticationManager.class);
-            OAuth2AuthorizationService oAuth2AuthorizationService = applicationContext.getBean(OAuth2AuthorizationService.class);
+        authorizationServerConfigurer.tokenEndpoint(tokenEndpoint ->
+                tokenEndpoint.accessTokenRequestConverter(new OAuth2ResourceOwnerPasswordCredentialsAuthenticationConverter())
+        );
+    }
 
-            configurer
-                    .tokenEndpoint(tokenEndpoint ->
-                            tokenEndpoint
-                                    .accessTokenRequestConverter(
-                                            new OAuth2ResourceOwnerPasswordCredentialsAuthenticationConverter())
-                                    .authenticationProvider(
-                                            new OAuth2ResourceOwnerPasswordCredentialsAuthenticationProvider(
-                                                    tokenGenerator, authenticationManager, oAuth2AuthorizationService))
-                    );
-        });
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        //we create token OAuth2ResourceOwnerPasswordCredentialsAuthenticationProvider here because OAuth2TokenGenerator
+        //is placed into shared objects only in {@code OAuth2TokenEndpointConfigurer#init} method.
+        OAuth2TokenGenerator<?> tokenGenerator = http.getSharedObject(OAuth2TokenGenerator.class);
+        ApplicationContext applicationContext = http.getSharedObject(ApplicationContext.class);
+        AuthenticationManager authenticationManager = applicationContext.getBean(AuthenticationManager.class);
+        OAuth2AuthorizationService oAuth2AuthorizationService = applicationContext.getBean(OAuth2AuthorizationService.class);
+
+        OAuth2ResourceOwnerPasswordCredentialsAuthenticationProvider authenticationProvider =
+                new OAuth2ResourceOwnerPasswordCredentialsAuthenticationProvider(tokenGenerator, authenticationManager, oAuth2AuthorizationService);
+        http.authenticationProvider(authenticationProvider);
     }
 }

7e1e856f801baed8b1c72112e0b90cfc89050a2a
aleksandrovpv <p.aleksandrov@haulmont.net>
2024-06-03 11:16:53 +0400

    MappingFieldComparator

---
+++ jmix-search/search/src/main/java/io/jmix/search/index/impl/MappingFieldComparator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.search.index.impl;
+
+import jakarta.validation.constraints.NotNull;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.Objects;
+
+import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
+
+@Component("search_MappingFieldComparator")
+public class MappingFieldComparator {
+    public static final String TYPE_KEY = "type";
+
+    public boolean isLeafField(@NotNull Map<String, Object> mapping) {
+        return mapping.get(TYPE_KEY) != null && mapping.get(TYPE_KEY) instanceof String;
+    }
+
+    public ComparingState compareLeafFields(@NotNull Map<String, Object> searchIndexMapping, @NotNull Map<String, Object> applicationMapping) {
+        if (isNull(searchIndexMapping) || isNull(applicationMapping)) throw new NullPointerException();
+        return searchIndexMapping.equals(applicationMapping) ? ComparingState.EQUAL : ComparingState.NOT_COMPATIBLE;
+    }
+}

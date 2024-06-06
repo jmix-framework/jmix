33c669646bd1d0fed5dadbb7ef94b6d2a22109af
Roman Pinyazhin <pinyazhin@haulmont.com>
2024-06-06 14:31:40 +0400

    GenericFilterEditAction is not included to Studio meta #3214

--- jmix-flowui/flowui-kit/src/main/java/io/jmix/flowui/kit/meta/action/StudioGenericFilterActions.java
+++ jmix-flowui/flowui-kit/src/main/java/io/jmix/flowui/kit/meta/action/StudioGenericFilterActions.java
@@ -106,6 +106,34 @@ public interface StudioGenericFilterActions {
     void copyAction();
 
     @StudioAction(
+            type = "genericFilter_edit",
+            description = "Edits current configuration",
+            classFqn = "io.jmix.flowui.action.genericfilter.GenericFilterEditAction",
+            icon = "io/jmix/flowui/kit/meta/icon/action/action.svg",
+            properties = {
+                    @StudioProperty(xmlAttribute = "actionVariant", type = StudioPropertyType.ENUMERATION,
+                            setMethod = "setVariant", classFqn = "io.jmix.flowui.kit.action.ActionVariant",
+                            defaultValue = "DEFAULT", options = {"DEFAULT", "PRIMARY", "DANGER", "SUCCESS"}),
+                    @StudioProperty(xmlAttribute = "description", type = StudioPropertyType.LOCALIZED_STRING),
+                    @StudioProperty(xmlAttribute = "enabled", type = StudioPropertyType.BOOLEAN, defaultValue = "true"),
+                    @StudioProperty(xmlAttribute = "icon", type = StudioPropertyType.ICON,
+                            setParameterFqn = "com.vaadin.flow.component.icon.Icon"),
+                    @StudioProperty(xmlAttribute = "id", type = StudioPropertyType.COMPONENT_ID,
+                            required = true, initialValue = "genericFilter_edit"),
+                    @StudioProperty(xmlAttribute = "shortcutCombination", type = StudioPropertyType.SHORTCUT_COMBINATION),
+                    @StudioProperty(xmlAttribute = "text", type = StudioPropertyType.LOCALIZED_STRING),
+                    @StudioProperty(xmlAttribute = "visible", type = StudioPropertyType.BOOLEAN, defaultValue = "true")
+            },
+            items = {
+                    @StudioPropertiesItem(xmlAttribute = "enabledByUiPermissions", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true"),
+                    @StudioPropertiesItem(xmlAttribute = "visibleByUiPermissions", type = StudioPropertyType.BOOLEAN,
+                            defaultValue = "true")
+            }
+    )
+    void editAction();
+
+    @StudioAction(
             type = "genericFilter_reset",
             description = "Resets current configuration",
             classFqn = "io.jmix.flowui.action.genericfilter.GenericFilterResetAction",

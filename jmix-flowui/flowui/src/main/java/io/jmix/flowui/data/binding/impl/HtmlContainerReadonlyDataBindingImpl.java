26e560544774311f08d4f35bd5f4e6bf728ab231
Ilia Grachev <i.grachev@haulmont.net>
2024-06-19 16:27:31 +0400

    Add permissions check for value source #3288

--- jmix-flowui/flowui/src/main/java/io/jmix/flowui/data/binding/impl/HtmlContainerReadonlyDataBindingImpl.java
+++ jmix-flowui/flowui/src/main/java/io/jmix/flowui/data/binding/impl/HtmlContainerReadonlyDataBindingImpl.java
@@ -18,13 +18,17 @@ package io.jmix.flowui.data.binding.impl;
 
 import com.vaadin.flow.component.HtmlContainer;
 import com.vaadin.flow.shared.Registration;
+import io.jmix.core.AccessManager;
 import io.jmix.core.MetadataTools;
-import io.jmix.core.common.datastruct.Pair;
 import io.jmix.core.common.event.Subscription;
 import io.jmix.core.entity.EntitySystemAccess;
+import io.jmix.core.metamodel.model.MetaPropertyPath;
+import io.jmix.flowui.accesscontext.UiEntityAttributeContext;
 import io.jmix.flowui.component.formatter.CollectionFormatter;
+import io.jmix.flowui.data.EntityValueSource;
 import io.jmix.flowui.data.ValueSource;
 import io.jmix.flowui.data.binding.HtmlContainerReadonlyDataBinding;
+import io.jmix.flowui.kit.component.ComponentUtils;
 import io.jmix.flowui.kit.component.formatter.Formatter;
 import io.jmix.flowui.model.CollectionContainer;
 import io.jmix.flowui.model.InstanceContainer;
@@ -34,9 +38,8 @@ import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.jmix.core.common.util.Preconditions.checkNotEmptyString;
 import static io.jmix.core.common.util.Preconditions.checkNotNullArgument;
@@ -47,11 +50,16 @@ public class HtmlContainerReadonlyDataBindingImpl implements HtmlContainerReadon
     private static final Logger log = LoggerFactory.getLogger(HtmlContainerReadonlyDataBindingImpl.class);
 
     protected MetadataTools metadataTools;
-    protected List<Pair<HtmlContainer, DataBindingListenerCleaner>> listenersRegistrations = new LinkedList<>();
+    protected CollectionFormatter collectionFormatter;
+    protected AccessManager accessManager;
 
+    protected Map<HtmlContainer, Runnable> listenersRegistrations = new HashMap<>();
 
-    public HtmlContainerReadonlyDataBindingImpl(MetadataTools metadataTools) {
+    public HtmlContainerReadonlyDataBindingImpl(MetadataTools metadataTools, CollectionFormatter collectionFormatter,
+                                                AccessManager accessManager) {
         this.metadataTools = metadataTools;
+        this.collectionFormatter = collectionFormatter;
+        this.accessManager = accessManager;
     }
 
     public void bind(HtmlContainer htmlComponent, ValueSource<?> valueSource) {
@@ -71,8 +79,9 @@ public class HtmlContainerReadonlyDataBindingImpl implements HtmlContainerReadon
         Registration registration = valueSource.addValueChangeListener(valueChangeEvent ->
                 updateHtmlContainerText(htmlContainer, formatter, valueChangeEvent.getValue()));
 
-        DataBindingListenerCleaner remove = registration::remove;
-        listenersRegistrations.add(new Pair<>(htmlContainer, remove));
+        listenersRegistrations.put(htmlContainer, registration::remove);
+
+        checkPermissions(htmlContainer, valueSource);
     }
 
     @Override
@@ -102,8 +111,6 @@ public class HtmlContainerReadonlyDataBindingImpl implements HtmlContainerReadon
                     updateHtmlContainerText(htmlContainer, formatter, itemPropertyChangeEvent.getValue());
                 }
         });
-        DataBindingListenerCleaner propertyChangeCleaner = propertyChangeSubscription::remove;
-        listenersRegistrations.add(new Pair<>(htmlContainer, propertyChangeCleaner));
 
         Subscription itemChangeSubscription = dataContainer.addItemChangeListener(itemChangeEvent -> {
             Object propertyValue = itemChangeEvent.getItem() != null
@@ -111,13 +118,16 @@ public class HtmlContainerReadonlyDataBindingImpl implements HtmlContainerReadon
                     : null;
             updateHtmlContainerText(htmlContainer, formatter, propertyValue);
         });
-        DataBindingListenerCleaner itemChangeCleaner = itemChangeSubscription::remove;
-        listenersRegistrations.add(new Pair<>(htmlContainer, itemChangeCleaner));
+
+        listenersRegistrations.put(htmlContainer, () -> {
+            propertyChangeSubscription.remove();
+            itemChangeSubscription.remove();
+        });
     }
 
     @Override
     public void bind(HtmlContainer htmlComponent, CollectionContainer<?> dataContainer) {
-        bind(htmlComponent, dataContainer, new CollectionFormatter(metadataTools));
+        bind(htmlComponent, dataContainer, collectionFormatter);
     }
 
     @Override
@@ -134,46 +144,47 @@ public class HtmlContainerReadonlyDataBindingImpl implements HtmlContainerReadon
         Subscription subscription = dataContainer.addCollectionChangeListener(collectionChangeEvent ->
                 updateHtmlContainerText(htmlContainer, formatter, dataContainer.getItems()));
 
-        DataBindingListenerCleaner dataBindingListenerCleaner = subscription::remove;
-        listenersRegistrations.add(new Pair<>(htmlContainer, dataBindingListenerCleaner));
+        listenersRegistrations.put(htmlContainer, subscription::remove);
     }
 
     public void unbind(HtmlContainer htmlContainer) {
         unbind(htmlContainer, false);
     }
 
-    protected void updateHtmlContainerText(HtmlContainer htmlContainer,
-                                           Formatter<Object> formatter,
-                                           @Nullable Object value) {
-        htmlContainer.setText(formatter.apply(value));
-    }
-
-    protected void updateHtmlContainerText(HtmlContainer htmlContainer,
-                                           Formatter<Collection<?>> formatter,
-                                           @Nullable Collection<?> value) {
+    protected <T> void updateHtmlContainerText(HtmlContainer htmlContainer,
+                                           Formatter<T> formatter,
+                                           @Nullable T value) {
         htmlContainer.setText(formatter.apply(value));
     }
 
     protected void unbind(HtmlContainer htmlContainer, boolean warnIfBound) {
         checkNotNullArgument(htmlContainer);
 
-        Iterator<Pair<HtmlContainer, DataBindingListenerCleaner>> iterator = listenersRegistrations.iterator();
-        while (iterator.hasNext()) {
-            Pair<HtmlContainer, DataBindingListenerCleaner> htmlContainerAndCleaner = iterator.next();
-            if (htmlContainer.equals(htmlContainerAndCleaner.getFirst())) {
-                htmlContainerAndCleaner.getSecond().remove();
-                iterator.remove();
+        Runnable removeRegistration = listenersRegistrations.remove(htmlContainer);
+        if (removeRegistration == null) {
+            return;
+        }
 
-                if (warnIfBound) {
-                    log.warn("Silent unbind. HtmlContainer with id = '{}' was already bounden with data source.",
-                            htmlContainer.getId());
-                }
-            }
+        removeRegistration.run();
+
+        if (warnIfBound) {
+            log.warn("Silent unbind. HtmlContainer with id = '{}' was already bounden with data source.",
+                    htmlContainer.getId());
         }
     }
 
-    @FunctionalInterface
-    protected interface DataBindingListenerCleaner {
-        void remove();
+    protected void checkPermissions(HtmlContainer htmlContainer, ValueSource<?> valueSource) {
+        if (valueSource instanceof EntityValueSource<?, ?> entityValueSource) {
+            MetaPropertyPath metaPropertyPath = entityValueSource.getMetaPropertyPath();
+
+            if (entityValueSource.isDataModelSecurityEnabled()) {
+                UiEntityAttributeContext attributeContext = new UiEntityAttributeContext(metaPropertyPath);
+                accessManager.applyRegisteredConstraints(attributeContext);
+
+                if (!attributeContext.canView()) {
+                    ComponentUtils.setVisible(htmlContainer, false);
+                }
+            }
+        }
     }
 }

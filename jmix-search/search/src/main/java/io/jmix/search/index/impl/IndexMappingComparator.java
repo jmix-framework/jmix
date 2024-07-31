6df07e42d53a36cbdbeabe7abed57c6f8724e5e3
aleksandrovpv <p.aleksandrov@haulmont.net>
2024-07-31 11:02:07 +0400

    New files adding

---
+++ jmix-search/search/src/main/java/io/jmix/search/index/impl/IndexMappingComparator.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.search.index.impl;
+
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component("search_SearchMappingComparator")
+public class IndexMappingComparator {
+
+    private final MappingFieldComparator mappingFieldComparator;
+
+    public IndexMappingComparator(MappingFieldComparator mappingFieldComparator) {
+        this.mappingFieldComparator = mappingFieldComparator;
+    }
+
+    public MappingComparingResult compare(Map<String, Object> searchIndexMapping, Map<String, Object> applicationMapping) {
+
+        if (!applicationMapping.keySet().containsAll(searchIndexMapping.keySet())) {
+            return MappingComparingResult.NOT_COMPATIBLE;
+        }
+
+        if (mappingFieldComparator.isLeafField(searchIndexMapping)) {
+            if (mappingFieldComparator.isLeafField(applicationMapping)) {
+                return mappingFieldComparator.compareLeafFields(searchIndexMapping, applicationMapping);
+            } else {
+                return MappingComparingResult.NOT_COMPATIBLE;
+            }
+        }
+
+        MappingComparingResult result = MappingComparingResult.EQUAL;
+        for (Map.Entry<String, Object> mapEntry : searchIndexMapping.entrySet()) {
+            if (!(mapEntry.getValue() instanceof Map)) {
+                return MappingComparingResult.NOT_COMPATIBLE;
+            }
+
+            MappingComparingResult currentResult = compare((Map<String, Object>) mapEntry.getValue(), (Map<String, Object>) applicationMapping.get(mapEntry.getKey()));
+            if (currentResult == MappingComparingResult.NOT_COMPATIBLE) return MappingComparingResult.NOT_COMPATIBLE;
+            if (currentResult == MappingComparingResult.CAN_BE_UPDATED && result != MappingComparingResult.CAN_BE_UPDATED) {
+                result = MappingComparingResult.CAN_BE_UPDATED;
+            }
+        }
+
+        if (result == MappingComparingResult.EQUAL && applicationMapping.size() > searchIndexMapping.size()) {
+            return MappingComparingResult.CAN_BE_UPDATED;
+        }
+
+        return result;
+    }
+
+    enum MappingComparingResult implements ConfigurationPartComparingResult{
+        EQUAL,
+        CAN_BE_UPDATED,
+        NOT_COMPATIBLE;
+
+        @Override
+        public boolean indexRecreatingIsRequired() {
+            return this == NOT_COMPATIBLE;
+        }
+
+        @Override
+        public boolean configurationUpdateIsRequired() {
+            return this == CAN_BE_UPDATED;
+        }
+    }
+}

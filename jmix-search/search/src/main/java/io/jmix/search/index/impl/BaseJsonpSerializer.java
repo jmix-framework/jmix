7dde5adf060bcd3032ae8f8e114514a299c0d984
aleksandrovpv <p.aleksandrov@haulmont.net>
2024-08-06 11:39:31 +0400

    JsonpSerializer

---
+++ jmix-search/search/src/main/java/io/jmix/search/index/impl/BaseJsonpSerializer.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.search.index.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public abstract class BaseJsonpSerializer<JsonpSerializableType, ClientType> implements JsonpSerializer<JsonpSerializableType, ClientType>{
+
+    protected final ObjectMapper objectMapper = new ObjectMapper();
+    @Override
+    public ObjectNode toObjectNode(JsonpSerializableType object, ClientType searchClient) {
+        JsonNode jsonNode = toJsonNode(object, searchClient);
+        if (jsonNode.isObject()) {
+            return (ObjectNode) jsonNode;
+        } else {
+            throw new RuntimeException("Unable to convert provided object to ObjectNode: JsonNode type is '" + jsonNode.getNodeType() + "'");
+        }
+    }
+
+    protected abstract JsonNode toJsonNode(JsonpSerializableType object, ClientType searchClient);
+
+}

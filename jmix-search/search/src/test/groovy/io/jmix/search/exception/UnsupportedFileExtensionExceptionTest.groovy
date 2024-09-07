23a2f66c84adea7d43bd0d0da1edc85c2a81e68b
aleksandrovpv <p.aleksandrov@haulmont.net>
2024-09-07 19:39:37 +0400

    UnsupportedFileExtensionExceptionTest

---
+++ jmix-search/search/src/test/groovy/io/jmix/search/exception/UnsupportedFileExtensionExceptionTest.groovy
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.search.exception
+
+import spock.lang.Specification
+
+class UnsupportedFileExtensionExceptionTest extends Specification {
+
+
+    public static final String MESSAGE_1 = "The file the-file-with-not-supported-extension.sql with 'sql' extension " +
+            "is not supported. " +
+            "Only following file extensions are supported pdf, doc, xls, docx, xlsx, odt, ods, rtf, rtf."
+    public static final String MESSAGE_2 = "The file another-file.smt with 'smt' extension is not supported. " +
+            "Only following file extensions are supported pdf, doc, xls, docx, xlsx, odt, ods, rtf, rtf."
+
+    def "message test"(){
+        when:
+        def exception = new UnsupportedFileExtensionException(a)
+
+        then:
+        exception.getMessage() == b
+
+        where:
+        a | b
+        "the-file-with-not-supported-extension.sql" | MESSAGE_1
+        "another-file.smt" | MESSAGE_2
+    }
+}

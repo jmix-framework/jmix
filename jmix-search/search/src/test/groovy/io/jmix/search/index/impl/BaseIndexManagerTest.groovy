db4be7aab2d9611a80ad26dc62db61556e24da1d
aleksandrovpv <p.aleksandrov@haulmont.net>
2024-09-10 15:41:19 +0400

    BaseIndexManagerTest

--- jmix-search/search/src/test/groovy/io/jmix/search/index/impl/BaseIndexManagerTest.groovy
+++ jmix-search/search/src/test/groovy/io/jmix/search/index/impl/BaseIndexManagerTest.groovy
@@ -18,8 +18,8 @@ package io.jmix.search.index.impl
 
 import io.jmix.search.SearchProperties
 import io.jmix.search.index.IndexConfiguration
-import io.jmix.search.index.IndexSchemaManagementStrategy
 import io.jmix.search.index.IndexSynchronizationStatus
+import io.jmix.search.index.mapping.IndexMappingConfiguration
 import spock.lang.Specification
 
 import static io.jmix.search.index.IndexSchemaManagementStrategy.*
@@ -30,7 +30,7 @@ class BaseIndexManagerTest extends Specification {
     public static final String INDEX_NAME = "some_index_name"
     public static final String ENTITY_NAME = "SomeEntityName"
 
-    def "SynchronizeIndexSchema1"() {
+    def "synchronizeIndexSchema. The giving configuration couldn't be null"() {
         given:
         BaseIndexManager indexManager = new BaseIndexManagerTestImpl(null, null, null, null, null)
 
@@ -39,40 +39,6 @@ class BaseIndexManagerTest extends Specification {
 
         then:
         thrown(IllegalArgumentException)
-    }
-
-    def "if index is missing and the strategy doesn't allow to create index the index should be marked as MISSED"() {
-        given:
-        SearchProperties searchPropertiesMock = Mock()
-        searchPropertiesMock.getIndexSchemaManagementStrategy() >> strategy
-
-        and:
-        IndexStateRegistry indexStateRegistry = Mock()
-
-        and:
-        BaseIndexManager indexManager = new BaseIndexManagerTestImpl(null, indexStateRegistry, searchPropertiesMock, null, null)
-        BaseIndexManager indexManagerSpy = Spy(indexManager)
-        indexManagerSpy.isIndexExist(INDEX_NAME) >> false
-
-
-        and:
-        IndexConfiguration indexConfigurationMock = Mock()
-        indexConfigurationMock.getIndexName() >> INDEX_NAME
-        indexConfigurationMock.getEntityName() >> ENTITY_NAME
-
-
-        when:
-        IndexSynchronizationStatus status = indexManagerSpy.synchronizeIndexSchema(indexConfigurationMock)
-
-        then:
-        status == resultStatus
-
-        0 * indexStateRegistry.markIndexAsAvailable(ENTITY_NAME)
-        0 * indexStateRegistry.markIndexAsUnavailable(ENTITY_NAME)
-
-        where:
-        strategy                           | resultStatus
-        IndexSchemaManagementStrategy.NONE | MISSING
     }
 
     def "if index is missing and the strategy allows to create index the attempt to create index should be performed"() {
@@ -104,6 +70,7 @@ class BaseIndexManagerTest extends Specification {
 
         where:
         strategy           | creationResult | resultStatus | markAsAvailableExecutes | markAsUnavailableExecutes
+        NONE               | null           | MISSING      | 0                       | 0
         CREATE_ONLY        | true           | CREATED      | 1                       | 0
         CREATE_ONLY        | false          | MISSING      | 0                       | 1
         CREATE_OR_RECREATE | true           | CREATED      | 1                       | 0
@@ -147,15 +114,112 @@ class BaseIndexManagerTest extends Specification {
         status == resultStatus
         markAsAvailableExecutes * indexStateRegistry.markIndexAsAvailable(ENTITY_NAME)
         markAsUnavailableExecutes * indexStateRegistry.markIndexAsUnavailable(ENTITY_NAME)
+        recreatingExecutes * indexManagerSpy.recreateIndex(indexConfigurationMock)
 
         where:
-        strategy           | droppingResult | creatingResult | resultStatus | markAsAvailableExecutes | markAsUnavailableExecutes
-        NONE               | null           | null           | IRRELEVANT   | 0                       | 1
-        CREATE_ONLY        | null           | null           | IRRELEVANT   | 0                       | 1
-        CREATE_OR_UPDATE   | null           | null           | IRRELEVANT   | 0                       | 1
-        CREATE_OR_RECREATE | true           | true           | RECREATED    | 1                       | 0
-        CREATE_OR_RECREATE | true           | false          | IRRELEVANT   | 0                       | 1
-        CREATE_OR_RECREATE | false          | null           | IRRELEVANT   | 0                       | 1
+        strategy           | droppingResult | creatingResult | resultStatus | recreatingExecutes | markAsAvailableExecutes | markAsUnavailableExecutes
+        NONE               | null           | null           | IRRELEVANT   | 0                  | 0                       | 1
+        CREATE_ONLY        | null           | null           | IRRELEVANT   | 0                  | 0                       | 1
+        CREATE_OR_UPDATE   | null           | null           | IRRELEVANT   | 0                  | 0                       | 1
+        CREATE_OR_RECREATE | true           | true           | RECREATED    | 1                  | 1                       | 0
+        CREATE_OR_RECREATE | true           | false          | IRRELEVANT   | 1                  | 0                       | 1
+        CREATE_OR_RECREATE | false          | null           | IRRELEVANT   | 1                  | 0                       | 1
+    }
+
+    def "index exists but index update required"() {
+        given:
+        SearchProperties searchPropertiesMock = Mock()
+        searchPropertiesMock.getIndexSchemaManagementStrategy() >> strategy
+
+        and:
+        IndexMappingConfiguration mappingConfiguration = Mock()
+        IndexConfiguration indexConfigurationMock = Mock()
+        indexConfigurationMock.getMapping() >> mappingConfiguration
+        indexConfigurationMock.getIndexName() >> INDEX_NAME
+        indexConfigurationMock.getEntityName() >> ENTITY_NAME
+
+        and:
+        IndexStateRegistry indexStateRegistry = Mock()
+
+        and:
+        ConfigurationComparingResult comparingResult = Mock()
+        comparingResult.isIndexRecreatingRequired() >> false
+        comparingResult.isConfigurationUpdateRequired() >> true
+        comparingResult.isMappingUpdateRequired() >> isMappingUpdateRequired
+
+        and:
+        IndexConfigurationComparator configurationComparator = Mock()
+        configurationComparator.compareConfigurations(indexConfigurationMock) >> comparingResult
+
+        and:
+        BaseIndexManager indexManager = new BaseIndexManagerTestImpl(null, indexStateRegistry, searchPropertiesMock, configurationComparator, null)
+        BaseIndexManager indexManagerSpy = Spy(indexManager)
+        indexManagerSpy.isIndexExist(INDEX_NAME) >> true
+
+        indexManagerSpy.putMapping(INDEX_NAME, mappingConfiguration) >> putMappingResult
+
+        when:
+        IndexSynchronizationStatus status = indexManagerSpy.synchronizeIndexSchema(indexConfigurationMock)
+
+        then:
+        status == resultStatus
+        markAsAvailableExecutes * indexStateRegistry.markIndexAsAvailable(ENTITY_NAME)
+        markAsUnavailableExecutes * indexStateRegistry.markIndexAsUnavailable(ENTITY_NAME)
+
+        where:
+        strategy           | isMappingUpdateRequired | putMappingResult | resultStatus | markAsAvailableExecutes | markAsUnavailableExecutes
+        NONE               | null                    | null             | IRRELEVANT   | 0                       | 1
+        CREATE_ONLY        | null                    | null             | IRRELEVANT   | 0                       | 1
+        CREATE_OR_UPDATE   | true                    | true             | UPDATED      | 1                       | 0
+        CREATE_OR_UPDATE   | true                    | false            | IRRELEVANT   | 0                       | 1
+        CREATE_OR_RECREATE | true                    | true             | UPDATED      | 1                       | 0
+        CREATE_OR_RECREATE | true                    | false            | IRRELEVANT   | 0                       | 1
+//These cases are not supported yet. The exception trowing is checked in the next test.
+//        CREATE_OR_UPDATE | false
+//        CREATE_OR_RECREATE | false
+    }
+
+    def "settings update don't supported"() {
+        given:
+        SearchProperties searchPropertiesMock = Mock()
+        searchPropertiesMock.getIndexSchemaManagementStrategy() >> strategy
+
+        and:
+        IndexMappingConfiguration mappingConfiguration = Mock()
+        IndexConfiguration indexConfigurationMock = Mock()
+        indexConfigurationMock.getMapping() >> mappingConfiguration
+        indexConfigurationMock.getIndexName() >> INDEX_NAME
+        indexConfigurationMock.getEntityName() >> ENTITY_NAME
+
+        and:
+        IndexStateRegistry indexStateRegistry = Mock()
+
+        and:
+        ConfigurationComparingResult comparingResult = Mock()
+        comparingResult.isIndexRecreatingRequired() >> false
+        comparingResult.isConfigurationUpdateRequired() >> true
+        comparingResult.isMappingUpdateRequired() >> isMappingUpdateRequired
+
+        and:
+        IndexConfigurationComparator configurationComparator = Mock()
+        configurationComparator.compareConfigurations(indexConfigurationMock) >> comparingResult
+
+        and:
+        BaseIndexManager indexManager = new BaseIndexManagerTestImpl(null, indexStateRegistry, searchPropertiesMock, configurationComparator, null)
+        BaseIndexManager indexManagerSpy = Spy(indexManager)
+        indexManagerSpy.isIndexExist(INDEX_NAME) >> true
+
+        when:
+        indexManagerSpy.synchronizeIndexSchema(indexConfigurationMock)
+
+        then:
+        def exception = thrown(IllegalStateException)
+        exception.getMessage() == "Only index mapping update is already supported."
+
+        where:
+        strategy           | isMappingUpdateRequired
+        CREATE_OR_UPDATE   | false
+        CREATE_OR_RECREATE | false
     }
 
 }

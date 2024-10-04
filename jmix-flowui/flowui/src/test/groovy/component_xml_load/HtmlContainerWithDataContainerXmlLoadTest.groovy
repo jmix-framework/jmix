675b2e637dd49d9eed7206a70ade54212e05603d
Dmitry Kremnev <kremnev.dima@mail.ru>
2024-10-04 17:05:44 +0400

    Extend baseHtmlDescription from htmlContainer in layout.xsd jmix-framework/jmix#3745

--- jmix-flowui/flowui/src/test/groovy/component_xml_load/HtmlContainerWithDataContainerXmlLoadTest.groovy
+++ jmix-flowui/flowui/src/test/groovy/component_xml_load/HtmlContainerWithDataContainerXmlLoadTest.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Haulmont.
+ * Copyright 2024 Haulmont.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,11 +16,9 @@
 
 package component_xml_load
 
-
 import com.vaadin.flow.component.HtmlContainer
 import component_xml_load.screen.HtmlDataContainerView
 import io.jmix.core.DataManager
-import io.jmix.core.SaveContext
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
@@ -40,13 +38,10 @@ class HtmlContainerWithDataContainerXmlLoadTest extends FlowuiTestSpecification 
     void setup() {
         registerViewBasePackages("component_xml_load.screen")
 
-        def saveContext = new SaveContext()
-
-        def foo = dataManager.create(Foo)
+        def foo = dataManager.create Foo
         foo.name = "Foo"
 
-        saveContext.saving(foo)
-        dataManager.save(saveContext)
+        dataManager.save foo
     }
 
     @Override
@@ -56,17 +51,15 @@ class HtmlContainerWithDataContainerXmlLoadTest extends FlowuiTestSpecification 
 
     def "Load #container with DataContainer from XML"() {
         when: "Open the HtmlView"
-        def htmlView = navigateToView(HtmlDataContainerView.class)
+        def htmlView = navigateToView HtmlDataContainerView
 
-        then: "#container dataContainer attributes will be loaded and data will be binded"
+        then: "#container dataContainer attributes will be loaded and data will be bound"
         def htmlContainer = htmlView."${container}Id" as HtmlContainer
 
-        verifyAll(htmlContainer) {
-            htmlContainer.getText() == "Foo"
-        }
+        htmlContainer.text == "Foo"
 
         where:
-        container << ["article", "aside", "div", "emphasis", "footer", "h1",
+        container << ["article", "aside", "descriptionList", "term", "description", "div", "emphasis", "footer", "h1",
                       "h2", "h3", "h4", "h5", "h6", "header", "listItem", "p", "pre", "section", "span",
                       "unorderedList", "anchor", "htmlObject", "label", "main", "nav", "orderedList"]
     }

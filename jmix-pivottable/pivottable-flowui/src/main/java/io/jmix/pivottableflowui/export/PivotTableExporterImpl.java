4a6c5b0d2ced43482a8083c9d1990a62f058e0aa
Ilia Grachev <i.grachev@haulmont.net>
2024-10-28 18:29:38 +0400

    Create PivotTableExcelExporter using ApplicationContext 3820

--- jmix-pivottable/pivottable-flowui/src/main/java/io/jmix/pivottableflowui/export/PivotTableExporterImpl.java
+++ jmix-pivottable/pivottable-flowui/src/main/java/io/jmix/pivottableflowui/export/PivotTableExporterImpl.java
@@ -23,7 +23,11 @@ import io.jmix.pivottableflowui.component.PivotTable;
 import io.jmix.pivottableflowui.component.PivotTableUtils;
 import io.jmix.pivottableflowui.export.model.PivotData;
 import io.jmix.pivottableflowui.kit.component.model.Renderer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
@@ -36,22 +40,30 @@ import java.util.Set;
  */
 @Component("pvttbl_PivotTableExporter")
 @Scope(BeanDefinition.SCOPE_PROTOTYPE)
-public class PivotTableExporterImpl implements PivotTableExporter {
+public class PivotTableExporterImpl implements PivotTableExporter, ApplicationContextAware, InitializingBean {
 
     public static final Set<Renderer> supportedRenderers = Collections.unmodifiableSet(Sets.newHashSet(
             Renderer.TABLE, Renderer.TABLE_BAR_CHART, Renderer.HEATMAP, Renderer.ROW_HEATMAP, Renderer.COL_HEATMAP));
 
+    protected ApplicationContext applicationContext;
     protected PivotTableExcelExporter excelExporter;
     protected PivotTable<?> pivotTable;
     protected String fileName;
 
-    public PivotTableExporterImpl(PivotTable<?> pivotTable, PivotTableExcelExporter exporter) {
+    public PivotTableExporterImpl(PivotTable<?> pivotTable) {
         Preconditions.checkNotNullArgument(pivotTable);
-        Preconditions.checkNotNullArgument(exporter);
 
         this.pivotTable = pivotTable;
-        this.excelExporter = exporter;
-        this.excelExporter.init(pivotTable);
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        this.excelExporter = applicationContext.getBean(PivotTableExcelExporter.class, pivotTable);
     }
 
     @Override

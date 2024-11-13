759a47b17dd95b0a8950b987ea7f232287878a84
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-11-13 10:56:36 +0400

    Amend UI minimal policies (3580)

--- jmix-templates/content/project/application-kotlin/src/main/kotlin/${project_rootPath}/security/UiMinimalRole.kt
+++ jmix-templates/content/project/application-kotlin/src/main/kotlin/${project_rootPath}/security/UiMinimalRole.kt
@@ -1,17 +1,13 @@
 package ${project_rootPackage}.security
 
-import io.jmix.core.entity.KeyValueEntity
-import io.jmix.security.model.EntityAttributePolicyAction
-import io.jmix.security.model.EntityPolicyAction
 import io.jmix.security.model.SecurityScope
-import io.jmix.security.role.annotation.EntityAttributePolicy
-import io.jmix.security.role.annotation.EntityPolicy
 import io.jmix.security.role.annotation.ResourceRole
 import io.jmix.security.role.annotation.SpecificPolicy
+import io.jmix.securityflowui.role.UiMinimalPolicies
 import io.jmix.securityflowui.role.annotation.ViewPolicy
 
 @ResourceRole(name = "UI: minimal access", code = UiMinimalRole.CODE, scope = [SecurityScope.UI])
-interface UiMinimalRole {
+interface UiMinimalRole : UiMinimalPolicies {
 
     companion object {
         const val CODE = "ui-minimal"
@@ -23,12 +19,4 @@ interface UiMinimalRole {
     @ViewPolicy(viewIds = ["${normalizedPrefix_underscore}LoginView"])
     @SpecificPolicy(resources = ["ui.loginToUi"])
     fun login()
-
-    @EntityPolicy(entityClass = KeyValueEntity::class, actions = [EntityPolicyAction.READ])
-    @EntityAttributePolicy(
-            entityClass = KeyValueEntity::class,
-            attributes = ["*"],
-            action = EntityAttributePolicyAction.VIEW
-    )
-    fun keyValueEntity()
 }

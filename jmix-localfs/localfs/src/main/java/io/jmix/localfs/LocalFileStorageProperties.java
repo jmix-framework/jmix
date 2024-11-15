2bef44fe50f4538fb70b28924a69706309d162c4
Ivan Gavrilov <i.gavrilov@haulmont.com>
2024-11-15 18:13:22 +0400

    Backport REST files fixes to 1.6 3836

--- jmix-localfs/localfs/src/main/java/io/jmix/localfs/LocalFileStorageProperties.java
+++ jmix-localfs/localfs/src/main/java/io/jmix/localfs/LocalFileStorageProperties.java
@@ -18,6 +18,8 @@ package io.jmix.localfs;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+import org.springframework.util.unit.DataSize;
 
 @ConfigurationProperties(prefix = "jmix.localfs")
 @ConstructorBinding
@@ -28,9 +30,16 @@ public class LocalFileStorageProperties {
      */
     String storageDir;
 
+    /**
+     * Maximum allowable file size.
+     */
+    DataSize maxFileSize;
+
     public LocalFileStorageProperties(
-            String storageDir) {
+            String storageDir,
+            @DefaultValue("100MB") DataSize maxFileSize) {
         this.storageDir = storageDir;
+        this.maxFileSize = maxFileSize;
     }
 
     /**
@@ -38,5 +47,12 @@ public class LocalFileStorageProperties {
      */
     public String getStorageDir() {
         return storageDir;
+    }
+
+    /**
+     * @see #maxFileSize
+     */
+    public DataSize getMaxFileSize() {
+        return maxFileSize;
     }
 }

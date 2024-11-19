be68dffdce6df7fa5652195e41704184d543b918
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-11-19 11:00:41 +0400

    Amend UI minimal policies #3580 (#3861)
    (cherry picked from commit bcdd4e1c89a1fa528b3066dd2b87313548b8758b)

---
+++ jmix-security/security-flowui/src/main/java/io/jmix/securityflowui/role/UiMinimalPolicies.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.securityflowui.role;
+
+
+import io.jmix.core.entity.KeyValueEntity;
+import io.jmix.security.model.EntityAttributePolicyAction;
+import io.jmix.security.model.EntityPolicyAction;
+import io.jmix.security.role.annotation.EntityAttributePolicy;
+import io.jmix.security.role.annotation.EntityPolicy;
+import io.jmix.securityflowui.role.annotation.ViewPolicy;
+
+public interface UiMinimalPolicies {
+
+    @ViewPolicy(viewIds = {"inputDialog", "multiValueSelectDialog"})
+    void systemDialogs();
+
+    @EntityPolicy(entityClass = KeyValueEntity.class, actions = EntityPolicyAction.READ)
+    @EntityAttributePolicy(entityClass = KeyValueEntity.class, attributes = "*", action = EntityAttributePolicyAction.VIEW)
+    void keyValueEntity();
+}

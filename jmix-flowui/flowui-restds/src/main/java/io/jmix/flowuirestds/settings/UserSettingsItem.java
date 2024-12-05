9baa7c349188658a400964e587e9b99f5beecd74
Konstantin Krivopustov <krivopustov@haulmont.com>
2024-12-05 19:06:03 +0400

    Refactor database-related dependencies: done for GenericFilter and Settings

---
+++ jmix-flowui/flowui-restds/src/main/java/io/jmix/flowuirestds/settings/UserSettingsItem.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2020 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.jmix.flowuirestds.settings;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.JmixId;
+import io.jmix.core.entity.annotation.SystemLevel;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import java.util.UUID;
+
+@JmixEntity(name = "flowui_UserSettingsItem")
+@SystemLevel
+public class UserSettingsItem {
+
+    @JmixId
+    @JmixGeneratedValue
+    protected UUID id;
+
+    private String username;
+
+    private String key;
+
+    private String value;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}

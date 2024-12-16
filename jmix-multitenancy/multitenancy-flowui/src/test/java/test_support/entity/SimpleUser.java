a3ffa1ffbbbf09d1d23e10c7f573c9a06d0b5c71
Roman Pinyazhin <pinyazhin@haulmont.com>
2024-12-16 09:44:31 +0400

    @TenantId from base class is ignored (3969) #3998

---
+++ jmix-multitenancy/multitenancy-flowui/src/test/java/test_support/entity/SimpleUser.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2024 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test_support.entity;
+
+import io.jmix.core.annotation.Secret;
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.entity.annotation.SystemLevel;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import io.jmix.security.authentication.JmixUserDetails;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.UUID;
+
+@JmixEntity
+@Entity
+@Table(name = "SIMPLE_USER", indexes = {
+        @Index(name = "IDX_SIMPLE_USER__ON_USERNAME", columnList = "USERNAME", unique = true)
+})
+public class SimpleUser extends AbstractTenantUser implements JmixUserDetails {
+
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @Column(name = "USERNAME", nullable = false)
+    private String username;
+
+    @Secret
+    @SystemLevel
+    @Column(name = "PASSWORD")
+    private String password;
+
+    @Transient
+    protected Collection<? extends GrantedAuthority> authorities;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities != null ? authorities : Collections.emptyList();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}

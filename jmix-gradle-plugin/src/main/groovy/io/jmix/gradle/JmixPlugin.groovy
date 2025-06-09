31824f085e02822211c2bbd5d1ff6792ad7b77c7
Dmitry Taimanov <d.taimanov@haulmont.com>
2025-06-09 15:58:21 +0400

    Update Jmix 2.x eclipselink fork to 4.0.6 jmix-framework/jmix#4278

--- jmix-gradle-plugin/src/main/groovy/io/jmix/gradle/JmixPlugin.groovy
+++ jmix-gradle-plugin/src/main/groovy/io/jmix/gradle/JmixPlugin.groovy
@@ -66,7 +66,7 @@ class JmixPlugin implements Plugin<Project> {
 
             if (project.jmix.entitiesEnhancing.enabled) {
                 project.configurations.create('enhancing')
-                project.dependencies.add('enhancing', 'org.eclipse.persistence:org.eclipse.persistence.jpa:4.0.2-3-jmix')
+                project.dependencies.add('enhancing', 'org.eclipse.persistence:org.eclipse.persistence.jpa:4.0.6-2-jmix')
 
                 if (javaPlugin) {
                     project.tasks.findByName('compileJava').doLast(new EnhancingAction('main'))

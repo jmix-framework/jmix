3cc254cb11e9361ac7b29dff88569effa41c81ab
Dmitry Panov <de.panov@haulmont.com>
2025-06-18 13:03:46 +0300

    Changes were made in accordance with the proposed edits: - Comments - showUuidFileds description

--- jmix-appsettings/appsettings-flowui/src/main/java/io/jmix/appsettingsflowui/AppSettingsUiProperties.java
+++ jmix-appsettings/appsettings-flowui/src/main/java/io/jmix/appsettingsflowui/AppSettingsUiProperties.java
@@ -1,18 +1,18 @@
-///*
-// * Copyright 2025 Haulmont.
-// *
-// * Licensed under the Apache License, Version 2.0 (the "License");
-// * you may not use this file except in compliance with the License.
-// * You may obtain a copy of the License at
-// *
-// *     http://www.apache.org/licenses/LICENSE-2.0
-// *
-// * Unless required by applicable law or agreed to in writing, software
-// * distributed under the License is distributed on an "AS IS" BASIS,
-// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// * See the License for the specific language governing permissions and
-// * limitations under the License.
-// */
+/*
+ * Copyright 2025 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package io.jmix.appsettingsflowui;
 
@@ -22,7 +22,7 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 @ConfigurationProperties(prefix = "jmix.appsettings.ui")
 public class AppSettingsUiProperties {
     /**
-     * Property for displaying the uuid field in the Application settings
+     * Defines whether uuid fields are displayed in the Application Settings view
      */
     final Boolean showUuidFields;
 

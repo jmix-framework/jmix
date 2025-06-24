d833965fa915f20cb7969ba0000d4136e5913550
budarov <a.budarov@haulmont.com>
2025-06-24 12:41:19 +0400

    more javadocs for enums used in annotations #667

--- jmix-reports/reports/src/main/java/io/jmix/reports/entity/CustomTemplateDefinedBy.java
+++ jmix-reports/reports/src/main/java/io/jmix/reports/entity/CustomTemplateDefinedBy.java
@@ -17,12 +17,44 @@
 package io.jmix.reports.entity;
 
 import io.jmix.core.metamodel.datatype.EnumClass;
+import io.jmix.reports.ReportsProperties;
 import io.jmix.reports.yarg.formatters.CustomReport;
 
+/**
+ * Determines how a custom report generation is defined and invoked.
+ * Used by custom report templates.
+ * @see io.jmix.reports.annotation.CustomTemplateParameters
+ * @see ReportTemplate
+ */
 public enum CustomTemplateDefinedBy implements EnumClass<Integer> {
+    /**
+     * Custom template renderer is defined by a class located on the classpath.
+     * The class must implement {@link CustomReport}, and must have a zero-argument constructor.
+     * The class is lazily loaded, and its instance is created at the moment of report generation.
+     */
     CLASS(100),
+
+    /**
+     * Custom template renderer is implemented as a Groovy script.
+     */
     SCRIPT(200),
+
+    /**
+     * Renderer is implemented as a remote HTTP service.
+     * Calling the renderer is performed in two stages:
+     * <ol>
+     * <li>Groovy script is called to determine the URL, including query parameters</li>
+     * <li>command-line <code>curl</code> utility is used to invoke the remote service</li>
+     * </ol>
+     * <br/>
+     * CURL-related properties are applicable here.
+     *
+     * @see ReportsProperties#getCurlPath()
+     * @see ReportsProperties#getCurlParams()
+     * @see ReportsProperties#getCurlTimeout()
+     */
     URL(300),
+
     /**
      * Object implementing {@link CustomReport} provided by the user.
      */

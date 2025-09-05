27863f3f2027f80415a8745472690daed0c21b08
Gleb Gorelov <glebfox@gmail.com>
2025-09-05 20:25:11 +0400

    UserMenu component (4651)

--- jmix-templates/content/project/application/src/main/java/${project_rootPath}/view/main/MainView.java
+++ jmix-templates/content/project/application/src/main/java/${project_rootPath}/view/main/MainView.java
@@ -1,12 +1,122 @@
 package ${project_rootPackage}.view.main;
 
+import ${project_rootPackage}.entity.User;
+import com.google.common.base.Strings;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.avatar.Avatar;
+import com.vaadin.flow.component.avatar.AvatarVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.Messages;
+import io.jmix.core.usersubstitution.CurrentUserSubstitution;
+import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.app.main.StandardMainView;
+import io.jmix.flowui.view.Install;
 import io.jmix.flowui.view.ViewController;
 import io.jmix.flowui.view.ViewDescriptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Route("")
 @ViewController(id = "${normalizedPrefix_underscore}MainView")
 @ViewDescriptor(path = "main-view.xml")
 public class MainView extends StandardMainView {
+
+    @Autowired
+    private Messages messages;
+    @Autowired
+    private UiComponents uiComponents;
+    @Autowired
+    private CurrentUserSubstitution currentUserSubstitution;
+
+    @Install(to = "userMenu", subject = "buttonRenderer")
+    private Component userMenuButtonRenderer(final UserDetails userDetails) {
+        User user = (User) userDetails;
+
+        if (user == null) {
+            return null;
+        }
+
+        String userName = generateUserName(user);
+
+        Div content = uiComponents.create(Div.class);
+        content.setClassName("user-menu-button-content");
+
+        Avatar avatar = createAvatar(userName);
+
+        Span name = uiComponents.create(Span.class);
+        name.setText(userName);
+        name.setClassName("user-menu-text");
+
+        content.add(avatar, name);
+
+        if (isSubstituted(user)) {
+            Span subtext = uiComponents.create(Span.class);
+            subtext.setText(messages.getMessage("userMenu.substituted"));
+            subtext.setClassName("user-menu-subtext");
+
+            content.add(subtext);
+        }
+
+        return content;
+    }
+
+    @Install(to = "userMenu", subject = "headerRenderer")
+    private Component userMenuHeaderRenderer(final UserDetails userDetails) {
+        User user = (User) userDetails;
+
+        if (user == null) {
+            return null;
+        }
+
+        Div content = uiComponents.create(Div.class);
+        content.setClassName("user-menu-header-content");
+
+        String name = generateUserName(user);
+
+        Avatar avatar = createAvatar(name);
+        avatar.addThemeVariants(AvatarVariant.LUMO_LARGE);
+
+        Span text = uiComponents.create(Span.class);
+        text.setText(name);
+        text.setClassName("user-menu-text");
+
+        content.add(avatar, text);
+
+        if (name.equals(user.getUsername())) {
+            text.addClassNames("user-menu-text-subtext");
+        } else {
+            Span subtext = uiComponents.create(Span.class);
+            subtext.setText(user.getUsername());
+            subtext.setClassName("user-menu-subtext");
+
+            content.add(subtext);
+        }
+
+        return content;
+    }
+
+    private Avatar createAvatar(String fullName) {
+        Avatar avatar = uiComponents.create(Avatar.class);
+        avatar.setName(fullName);
+        avatar.getElement().setAttribute("tabindex", "-1");
+        avatar.setClassName("user-menu-avatar");
+
+        return avatar;
+    }
+
+    private String generateUserName(User user) {
+        String userName = String.format("%s %s",
+                        Strings.nullToEmpty(user.getFirstName()),
+                        Strings.nullToEmpty(user.getLastName()))
+                .trim();
+
+        return userName.isEmpty() ? user.getUsername() : userName;
+    }
+
+    private boolean isSubstituted(User user) {
+        UserDetails authenticatedUser = currentUserSubstitution.getAuthenticatedUser();
+        return user != null && !authenticatedUser.getUsername().equals(user.getUsername());
+    }
 }

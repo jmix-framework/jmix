d30d7b496e1fd01193cd0f196a2cb5c131a8ccfd
Dmitry Panov <de.panov@haulmont.com>
2025-10-15 23:11:56 +0300

    JPQL query state for the collection loader has been reverted to version 2.6.2 because the filter is not working correctly (rollback from 667 task state. Some requirements have not been finalized for working with report codes)
    ReportSource enum now correctly implements the EnumClass interface

--- jmix-reports/reports/src/main/java/io/jmix/reports/entity/ReportSource.java
+++ jmix-reports/reports/src/main/java/io/jmix/reports/entity/ReportSource.java
@@ -16,22 +16,44 @@
 
 package io.jmix.reports.entity;
 
+import io.jmix.core.metamodel.datatype.EnumClass;
+import org.springframework.lang.Nullable;
+
 /**
  * Defines where the report or report group comes from.
  */
-public enum ReportSource {
+public enum ReportSource implements EnumClass<String> {
 
     /**
      * Report or report group was defined in the project source code.
      * @see io.jmix.reports.annotation.ReportDef
      * @see io.jmix.reports.annotation.ReportGroupDef
      */
-    ANNOTATED_CLASS,
+    ANNOTATED_CLASS("annotatedClass"),
 
     /**
      * Report or report group is stored as persistent entity in the database.
      * @see Report
      * @see ReportGroup
      */
-    DATABASE
+    DATABASE("database");
+
+    private final String id;
+
+    ReportSource(String id) { this.id = id; }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Nullable
+    public static ReportSource fromId(String id) {
+        for (ReportSource item : ReportSource.values()) {
+            if (item.getId().equals(id)) {
+                return item;
+            }
+        }
+        return null;
+    }
 }

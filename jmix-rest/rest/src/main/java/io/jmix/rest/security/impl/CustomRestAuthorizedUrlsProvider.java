c08d7e5a8d2d989fedaa18c57417bd93f995ff36
Gleb Gorelov <glebfox@gmail.com>
2025-11-29 12:25:44 +0400

    Get rid of deprecated API #4866

---
+++ jmix-rest/rest/src/main/java/io/jmix/rest/security/impl/CustomRestAuthorizedUrlsProvider.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2021 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.jmix.rest.security.impl;
+
+import com.google.common.base.Splitter;
+import io.jmix.core.JmixModules;
+import io.jmix.securityresourceserver.requestmatcher.urlprovider.AnonymousUrlPatternsProvider;
+import io.jmix.securityresourceserver.requestmatcher.urlprovider.AuthenticatedUrlPatternsProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CustomRestAuthorizedUrlsProvider
+        implements AuthenticatedUrlPatternsProvider, AnonymousUrlPatternsProvider {
+
+    protected static final Splitter urlPatternSplitter = Splitter.on(",")
+            .omitEmptyStrings()
+            .trimResults();
+
+    @Autowired
+    private JmixModules jmixModules;
+
+    @Override
+    public List<String> getAuthenticatedUrlPatterns() {
+        List<String> urlPatterns = jmixModules.getPropertyValues("jmix.rest.authenticated-url-patterns");
+        return urlPatterns.stream()
+                .flatMap(s -> urlPatternSplitter.splitToList(s).stream())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> getAnonymousUrlPatterns() {
+        List<String> urlPatterns = jmixModules.getPropertyValues("jmix.rest.anonymous-url-patterns");
+        return urlPatterns.stream()
+                .flatMap(s -> urlPatternSplitter.splitToList(s).stream())
+                .collect(Collectors.toList());
+    }
+}

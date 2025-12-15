65ed7e122dbac81229a0277694687dd2e9a73f23
Dmitry Panov <de.panov@haulmont.com>
2025-12-15 17:10:51 +0300

    Missing Validation Feedback for Mandatory Report Code #4897

--- jmix-reports/reports-flowui/src/main/java/io/jmix/reportsflowui/view/report/ReportDetailView.java
+++ jmix-reports/reports-flowui/src/main/java/io/jmix/reportsflowui/view/report/ReportDetailView.java
@@ -659,13 +659,13 @@ public class ReportDetailView extends StandardDetailView<Report> {
         Report editedReport = getEditedEntity();
         Optional<UUID> reportId;
 
-        if (reportCode == null) {
-            markFieldAndPreventSave(codeField, "detailsTab.codeField.isEmpty.text", event);
-            showNotificationIfAnotherTab(mainTabSheetDetailsTab, errors, codeField, "detailsTab.codeField.isEmpty.text");
-            return;
-        }
-
         try {
+            if (reportCode == null) {
+                markFieldAndPreventSave(codeField, "detailsTab.codeField.isEmpty.text", event);
+                showNotificationIfAnotherTab(mainTabSheetDetailsTab, errors, codeField, "detailsTab.codeField.isEmpty.text");
+                return;
+            }
+
             if (entityStates.isNew(editedReport)) {
                 if (reportRepository.existsReportByCode(reportCode)) {
                     markFieldAndPreventSave(codeField, "detailsTab.codeField.alreadyExists.text", event);

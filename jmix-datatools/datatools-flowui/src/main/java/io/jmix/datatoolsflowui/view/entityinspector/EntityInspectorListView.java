872d44ec5ddfde4f67086c19c59a82a9fc0ae245
Roman Pinyazhin <pinyazhin@haulmont.com>
2026-01-19 11:07:59 +0400

    EntityInspectorListView fails to export entities when they contain soft-deleted ones #4978

--- jmix-datatools/datatools-flowui/src/main/java/io/jmix/datatoolsflowui/view/entityinspector/EntityInspectorListView.java
+++ jmix-datatools/datatools-flowui/src/main/java/io/jmix/datatoolsflowui/view/entityinspector/EntityInspectorListView.java
@@ -1127,7 +1127,8 @@ public class EntityInspectorListView extends StandardListView<Object> {
             MetaClass metaClass = metadata.getClass(fetchPlan.getEntityClass());
             LoadContext<?> ctx = new LoadContext<>(metaClass)
                     .setIds(ids)
-                    .setFetchPlan(fetchPlan);
+                    .setFetchPlan(fetchPlan)
+                    .setHint("jmix.softDeletion", false);
 
             return dataManager.loadList(ctx);
         }

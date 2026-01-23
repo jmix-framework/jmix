65520db3dcef665955fe0f52abee8748224304da
Mikhail Fedoseev <m.fedoseev@haulmont.com>
2026-01-23 16:59:43 +0300

    Fix comment generation in template for detail view controller #4997
    (cherry picked from commit f8dd827c73864bd344e415ff3064f958c50b6c37)

--- jmix-templates/content/flowui/detail/controller.java
+++ jmix-templates/content/flowui/detail/controller.java
@@ -57,17 +57,17 @@ public class ${detailControllerName} extends StandardDetailView<${entity.classNa
 
     @Install(target = Target.DATA_CONTEXT)
     private Set<Object> saveDelegate(SaveContext saveContext) {
-        <%def compositeAttrs = ''
-          detailFetchPlan.orderedRootProperties.each {property ->
-              def propAttr = detailFetchPlan.entity.getAttribute(property.name)
-              if (propAttr != null && propAttr.hasAnnotation('Composition')) {
-                  compositeAttrs = compositeAttrs + property.name + ', '
-              }
-          }
-          if (compositeAttrs.length() > 0){
-              compositeAttrs = compositeAttrs.substring(0, compositeAttrs.length() - 2);
-              println """// ${entity.className} has the following @Composition attributes: $compositeAttrs.
-                       // Make sure they have CascadeType.ALL in @OneToMany annotation."""}
-        %>return Set.of(repository.save(getEditedEntity()));
+        <%
+        def compositeAttrs = []
+        detailFetchPlan.orderedRootProperties.each { property ->
+            def propAttr = detailFetchPlan.entity.getAttribute(property.name)
+            if (propAttr != null && propAttr.hasAnnotation('Composition')) {
+                    compositeAttrs << property.name
+            }
+        }
+        if (!compositeAttrs.isEmpty()) {
+            out.println("        // ${entity.className} has the following @Composition attributes: ${compositeAttrs.join(', ')}.")
+            out.println("        // Make sure they have CascadeType.ALL in @OneToMany annotation.")
+        }%>return Set.of(repository.save(getEditedEntity()));
     }<%}%>
 }

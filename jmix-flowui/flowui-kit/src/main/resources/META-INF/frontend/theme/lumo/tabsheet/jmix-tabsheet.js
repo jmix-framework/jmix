afa836e3216ad6a3bc70e3c6d59c4bb34a411ff6
Gleb Gorelov <glebfox@gmail.com>
2026-01-30 18:17:59 +0400

    Remove lumo js styles

--- jmix-flowui/flowui-kit/src/main/resources/META-INF/frontend/theme/lumo/tabsheet/jmix-tabsheet.js
+++
@@ -1,5 +0,0 @@
-// CAUTION: copied from @vaadin/tabsheet/theme/lumo/vaadin-tabsheet.js [last update Vaadin 24.9.0]
-import '@vaadin/tabs/theme/lumo/vaadin-tabs.js';
-import '@vaadin/scroller/theme/lumo/vaadin-scroller.js';
-import './jmix-tabsheet-styles.js';
-import '../../../src/tabsheet/jmix-tabsheet.js';

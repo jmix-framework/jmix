7ec24c211ee2655340e2df08d38ee38331223849
Ivan Gavrilov <i.gavrilov@haulmont.com>
2026-05-08 17:11:04 +0400

    Add Factor Authorities for user substitution 5267

--- jmix-core/core/src/main/java/io/jmix/core/security/impl/SubstitutedUserAuthenticationProvider.java
+++ jmix-core/core/src/main/java/io/jmix/core/security/impl/SubstitutedUserAuthenticationProvider.java
@@ -19,10 +19,17 @@ package io.jmix.core.security.impl;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.FactorGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class SubstitutedUserAuthenticationProvider implements AuthenticationProvider {
+
+    private static final String SUBSTITUTION_AUTHORITY = "FACTOR_SUBSTITUTION";
 
     private UserDetailsService userDetailsService;
 
@@ -35,13 +42,15 @@ public class SubstitutedUserAuthenticationProvider implements AuthenticationProv
         SubstitutedUserAuthenticationToken substitutedAuthentication = (SubstitutedUserAuthenticationToken) authentication;
 
         String substitutedUserName = (String) substitutedAuthentication.getSubstitutedPrincipal();
-        // TODO [SB4] FactorGrantedAuthority is missed on loading
         UserDetails substitutedUser = userDetailsService.loadUserByUsername(substitutedUserName);
+
+        Collection<? extends GrantedAuthority> authorities = getEffectiveAuthorities(substitutedAuthentication, substitutedUser);
 
         SubstitutedUserAuthenticationToken authenticated = new SubstitutedUserAuthenticationToken(
                 substitutedAuthentication,
                 substitutedUser,
-                substitutedUser.getAuthorities());
+                authorities
+        );
 
         return authenticated;
     }
@@ -49,5 +58,29 @@ public class SubstitutedUserAuthenticationProvider implements AuthenticationProv
     @Override
     public boolean supports(Class<?> authentication) {
         return SubstitutedUserAuthenticationToken.class.isAssignableFrom(authentication);
+    }
+
+    /**
+     * Merge authorities:
+     * <ul>
+     *     <li>Substituted user authorities</li>
+     *     <li>Original user factor authorities</li>
+     *     <li>Substitution factor authority</li>
+     * </ul>
+     *
+     * @param substitutedAuthentication token with original authorities
+     * @param substitutedUser           user details with substituted authorities
+     * @return merged authorities
+     */
+    protected Collection<? extends GrantedAuthority> getEffectiveAuthorities(SubstitutedUserAuthenticationToken substitutedAuthentication,
+                                                                             UserDetails substitutedUser) {
+        Collection<GrantedAuthority> mergedAuthorities = new ArrayList<>(substitutedUser.getAuthorities());
+        Collection<GrantedAuthority> originalAuthorities = substitutedAuthentication.getAuthorities();
+        substitutedAuthentication.getAuthorities().stream()
+                .filter(authority -> authority instanceof FactorGrantedAuthority)
+                .forEach(autority -> mergedAuthorities.add(autority));
+
+        mergedAuthorities.add(FactorGrantedAuthority.fromAuthority(SUBSTITUTION_AUTHORITY));
+        return mergedAuthorities;
     }
 }

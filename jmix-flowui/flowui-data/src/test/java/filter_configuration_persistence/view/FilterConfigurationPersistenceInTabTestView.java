a99bb218d751cc8d592b5bf4766a032b65e9f03f
Roman Pinyazhin <pinyazhin@haulmont.com>
2026-05-28 09:30:24 +0400

    GenericFilter does not load configurations if it is placed in a tab other than the first selected one (5315) #5319

---
+++ jmix-flowui/flowui-data/src/test/java/filter_configuration_persistence/view/FilterConfigurationPersistenceInTabTestView.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2026 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package filter_configuration_persistence.view;
+
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.component.genericfilter.GenericFilter;
+import io.jmix.flowui.component.tabsheet.JmixTabSheet;
+import io.jmix.flowui.view.StandardView;
+import io.jmix.flowui.view.ViewComponent;
+import io.jmix.flowui.view.ViewController;
+import io.jmix.flowui.view.ViewDescriptor;
+
+@Route("filter-configuration-persistence-in-tab-test-view")
+@ViewController("FilterConfigurationPersistenceInTabTestView")
+@ViewDescriptor("filter-configuration-persistence-in-tab-test-view.xml")
+public class FilterConfigurationPersistenceInTabTestView extends StandardView {
+
+    @ViewComponent
+    public JmixTabSheet tabSheet;
+
+    @ViewComponent
+    public Tab regularFiltersTab;
+
+    @ViewComponent
+    public Tab lazyFiltersTab;
+
+    @ViewComponent
+    public GenericFilter genericFilterInRegularTab;
+}

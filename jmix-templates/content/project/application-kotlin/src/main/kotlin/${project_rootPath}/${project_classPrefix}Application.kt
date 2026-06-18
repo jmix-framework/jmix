c816c99beffab26c4630c92b95f4706f72b74b60
Mikhail Fedoseev <m.fedoseev@haulmont.com>
2026-06-18 18:56:57 +0400

    Remove duplicated import in templates
    (cherry picked from commit c0f5ed48a90cc75c0e95caf290dd8fdc7a2af9ff)

--- jmix-templates/content/project/application-kotlin/src/main/kotlin/${project_rootPath}/${project_classPrefix}Application.kt
+++ jmix-templates/content/project/application-kotlin/src/main/kotlin/${project_rootPath}/${project_classPrefix}Application.kt
@@ -11,7 +11,6 @@ private String getThemeName(){
 import com.vaadin.flow.component.page.AppShellConfigurator
 import com.vaadin.flow.component.dependency.StyleSheet
 import com.vaadin.flow.component.page.Push
-import com.vaadin.flow.server.PWA
 import com.vaadin.flow.server.PWA;<%if("lumo".equals(getThemeName())) {%>
 import com.vaadin.flow.theme.lumo.Lumo;
 import io.jmix.flowui.theme.lumo.JmixLumo;<%} else {%>

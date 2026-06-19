d972251c187f174fec426da1d768b090b89e0c37
Gleb Gorelov <glebfox@gmail.com>
2026-06-19 15:55:47 +0400

    Update copyright jmix-framework/jmix_4816

--- jmix-flowui/flowui-kit/src/main/resources/META-INF/frontend/src/tabsheet/jmix-tabsheet.js
+++ jmix-flowui/flowui-kit/src/main/resources/META-INF/frontend/src/tabsheet/jmix-tabsheet.js
@@ -1,8 +1,19 @@
-/**
- * @license
- * Copyright (c) 2022 Vaadin Ltd.
- * This program is available under Apache License Version 2.0, available at https://vaadin.com/license/
+/*
+ * Copyright 2026 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 import '@vaadin/tabsheet/src/vaadin-tabsheet-scroller.js';
 import { html, LitElement } from 'lit';
 import { defineCustomElement } from '@vaadin/component-base/src/define.js';

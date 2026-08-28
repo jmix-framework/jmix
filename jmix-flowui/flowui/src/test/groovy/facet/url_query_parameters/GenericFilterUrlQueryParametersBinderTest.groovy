5828d5c997cc588f64eb7d5dc000b9f4f65e8f15
Pavel Aleksandrov <p.aleksandrov@haulmont.net>
2026-08-28 16:43:26 +0400

    GenericFilter: a condition restored from the URL is duplicated in the data loader and freezes its value #5624
    (cherry picked from commit cd5bf77933a68aeee80f1337dc13ca2f853f730c)

--- jmix-flowui/flowui/src/test/groovy/facet/url_query_parameters/GenericFilterUrlQueryParametersBinderTest.groovy
+++ jmix-flowui/flowui/src/test/groovy/facet/url_query_parameters/GenericFilterUrlQueryParametersBinderTest.groovy
@@ -20,10 +20,11 @@ import com.vaadin.flow.router.QueryParameters
 import facet.url_query_parameters.view.GenericFilterUrlQueryParamsTestView
 import io.jmix.flowui.component.filter.FilterComponent
 import io.jmix.flowui.component.propertyfilter.PropertyFilter
-import io.jmix.flowui.facet.UrlQueryParametersFacet
 import io.jmix.flowui.facet.urlqueryparameters.GenericFilterUrlQueryParametersBinder
 import org.springframework.boot.test.context.SpringBootTest
 import test_support.spec.FlowuiTestSpecification
+
+import static facet.url_query_parameters.TestGenericFilterUrlBinders.getBinder
 
 /**
  * Checks how {@link GenericFilterUrlQueryParametersBinder} deserializes property conditions from
@@ -108,12 +109,5 @@ class GenericFilterUrlQueryParametersBinderTest extends FlowuiTestSpecification 
         binder.updateState(qp)
 
         return screen.ownersFilter.currentConfiguration.rootLogicalFilterComponent.filterComponents
-    }
-
-    private GenericFilterUrlQueryParametersBinder getBinder(GenericFilterUrlQueryParamsTestView screen) {
-        UrlQueryParametersFacet facet = screen.urlQueryParameters
-        return facet.binders
-                .findAll { it instanceof GenericFilterUrlQueryParametersBinder }
-                .first() as GenericFilterUrlQueryParametersBinder
     }
 }

d9b5c71dcd7f4a5d9e45537bdc6d25f123fb1993
Ivan Gavrilov <i.gavrilov@haulmont.com>
2026-09-22 16:10:22 +0400

    DeletePolicyProcessor converts entity id with the main store's DbTypeConverter instead of the entity's own store - breaks delete/unlink for additional stores with a different DBMS #5710

---
+++ jmix-data/eclipselink/src/test/java/test_support/entity/multidb/Db1Terminal.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2026 Haulmont.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package test_support.entity.multidb;
+
+import io.jmix.core.DeletePolicy;
+import io.jmix.core.entity.annotation.OnDelete;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+import io.jmix.core.metamodel.annotation.Store;
+import test_support.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@JmixEntity
+@Entity(name = "test_Db1Terminal")
+@Table(name = "TERMINAL")
+@Store(name = "db1")
+public class Db1Terminal extends BaseEntity {
+
+    @Column(name = "NAME")
+    @InstanceName
+    private String name;
+
+    @OnDelete(DeletePolicy.UNLINK)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "AIRPORT_ID")
+    private Db1Airport airport;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Db1Airport getAirport() {
+        return airport;
+    }
+
+    public void setAirport(Db1Airport airport) {
+        this.airport = airport;
+    }
+}
